d8bc335caa766341267b557f4bef5d6fa92ba2d6
HansRobo <pythagora.yoshimoto@gmail.com>
2020-03-05 17:23:52 +0900

    [ci skip] Add files

---
+++ crane_bt_executor/include/crane_bt_executor/behavior_tree/multi_robot_sequence.hpp
@@ -0,0 +1,47 @@
+// Copyright (c) 2020 ibis-ssl
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+#ifndef CRANE_BT_EXECUTOR__BEHAVIOR_TREE__MULTI_ROBOT_SEQUENCE_HPP_
+#define CRANE_BT_EXECUTOR__BEHAVIOR_TREE__MULTI_ROBOT_SEQUENCE_HPP_
+
+#include "crane_bt_executor/composite/composite.hpp"
+#include "crane_bt_executor/behavior_tree/behavior_base.hpp"
+#include <crane_bt_executor/robot_io.hpp>
+
+
+/**
+ * 逐次実行．（先頭のものが成功するまで次にいかない）
+ * 全て成功すれば成功判定
+ * 途中で失敗が出れば失敗判定
+ */
+class MultiRobotBehavior : public Composite, BehaviorBase
+{
+public:
+  MultiRobotBehavior()
+  {
+    name = "MultiRobotBehavior";
+  }
+
+  Status run(WorldModel & world_model, RobotIO robot) override {}
+  void update(WorldModel & world_model){
+
+  }
+};
+#endif  // CRANE_BT_EXECUTOR__BEHAVIOR_TREE__MULTI_ROBOT_SEQUENCE_HPP_
---
+++ crane_bt_executor/include/crane_bt_executor/robot_io.hpp
@@ -0,0 +1,47 @@
+// Copyright (c) 2020 ibis-ssl
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+#ifndef CRANE_BT_EXECUTOR__ROBOT_IO_HPP_
+#define CRANE_BT_EXECUTOR__ROBOT_IO_HPP_
+
+#include <crane_bt_executor/utils/robot_command_builder.hpp>
+#include <crane_bt_executor/utils/world_model.hpp>
+
+#include <memory>
+
+struct RobotIO{
+  std::shared_ptr<RobotInfo> info;
+  std::shared_ptr<RobotCommandBuilder> builder;
+  bool extractRobotInfo(WorldModel & world_model, uint8_t id){
+    if(id < 0 || id >= world_model.ours.robots.size()){
+      return false;
+    }
+    info.reset(&world_model.ours.robots.at(id));
+    return true;
+  }
+  bool resetBuilder(uint8_t id){
+    builder = std::make_shared<RobotCommandBuilder>(id);
+    return true;
+  }
+  bool init(WorldModel & world_model, uint8_t id){
+    return extractRobotInfo(world_model, id) && resetBuilder(id);
+  }
+};
+#endif  // CRANE_BT_EXECUTOR__ROBOT_IO_HPP_
