638d1994123f51df8f93eb864172404ed104399d
HansRobo <pythagora.yoshimoto@gmail.com>
2020-03-31 12:29:02 +0900

    [bt_executor] implement update function of FSM

--- crane_bt_executor/include/crane_bt_executor/utils/finite_state_machine.hpp
+++ crane_bt_executor/include/crane_bt_executor/utils/finite_state_machine.hpp
@@ -25,6 +25,7 @@
 #include <functional>
 #include <map>
 #include <string>
+#include <optional>
 
 struct State
 {
@@ -39,7 +40,7 @@ struct State
 struct StateWithTransition
 {
   State state;
-  std::map<uint8_t, std::function<bool(WorldModel &)>> transitions;
+  std::map<std::string, std::function<bool(WorldModel &)>> transitions;
 };
 
 class FiniteStateMachine
@@ -72,13 +73,59 @@ public:
 
     return true;
   }
-  void update()
+  void update(WorldModel & world_model)
   {
     // TODO(HansRobo) : Imprement
+    decltype(states.find(current_state)) state;
+    try {
+      state = states.find(current_state);
+    } catch (std::out_of_range & oor) {return;}
 
+    // state update
+    if (state->second.state.on_update) {
+      state->second.state.on_update(world_model);
+    }
+
+    // check for transition
+    for (auto transition : state->second.transitions) {
+      bool result = transition.second(world_model);
+      if (result) {
+        auto name = transition.first;
+        // find next state
+        decltype(states.find(name)) next_state;
+        try {
+          next_state = states.find(name);
+        } catch (std::out_of_range & oor) {return;}
+
+        // transition
+        // finalize
+        if(state->second.state.on_exit){
+          state->second.state.on_exit(world_model);
+        }
+        // initialize
+        if(next_state->second.state.on_enter){
+          next_state->second.state.on_enter(world_model);
+        }
+        current_state = name;
+      }
+    }
+  }
+
+  void executeState(State & state)
+  {
+  }
+
+  bool transitionTo(std::string name){
+    decltype(states.find(name)) next_state;
+    try {
+      next_state = states.find(name);
+    } catch (std::out_of_range & oor) {return false;}
+
+    return true;
   }
 
 private:
   std::map<std::string, StateWithTransition> states;
+  std::string current_state = "Start";
 };
 #endif  // CRANE_BT_EXECUTOR__UTILS__FINITE_STATE_MACHINE_HPP_
