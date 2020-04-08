edd6546a70140c2f13c2af5eb2f9bf5f34bc4648
HansRobo <pythagora.yoshimoto@gmail.com>
2020-04-08 22:26:01 +0900

    [bt_executor] add VelocityPlanner

---
+++ crane_bt_executor/include/crane_bt_executor/utils/velocity_planner.hpp
@@ -0,0 +1,62 @@
+// Copyright (c) 2020 ibis-ssl
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+#ifndef CRANE_BT_EXECUTOR__UTILS__VELOCITY_PLANNER_HPP_
+#define CRANE_BT_EXECUTOR__UTILS__VELOCITY_PLANNER_HPP_
+
+#include "boost_geometry.hpp"
+
+class VelocityPlanner
+{
+public:
+  void initilize(float freq, float max_vel, float max_acc)
+  {
+    dt_sec_ = 1.f / freq;
+    max_velocity_ = max_vel;
+    max_accelaration_ = max_acc;
+  }
+  void update(float target_vel, Point current_pos, Point target_pos)
+  {
+    target_velocity_ = target_vel;
+
+    // if faster than the target, slow down when the limit has come.
+    if (current_velocity_ > target_velocity_) {
+      float dist = (current_pos - target_pos).norm();
+      float velocity_limit = std::sqrt(2 * max_accelaration_ * dist);
+      if (current_velocity_ > velocity_limit) {
+        current_velocity_ = velocity_limit;
+      }
+    } else {  // if slower than target, accelarate immediately.
+      current_velocity_ += max_accelaration_ * dt_sec_;
+      if (current_velocity_ > max_velocity_) {
+        current_velocity_ = max_velocity_;
+      }
+    }
+  }
+  float getVelocity() {return current_velocity_;}
+
+private:
+  float dt_sec_;
+  float max_velocity_;
+  float max_accelaration_;
+  float current_velocity_;
+  float target_velocity_;
+};
+#endif  // CRANE_BT_EXECUTOR__UTILS__VELOCITY_PLANNER_HPP_
