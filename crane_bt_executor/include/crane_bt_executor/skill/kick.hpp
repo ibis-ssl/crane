cfe7b60569b926fdd22bbe6894b75702391bfe39
Kotaro Yoshimoto <pythagora.yoshimoto@gmail.com>
2020-05-06 19:10:05 +0900

    [bt_executor] add Kick skill

---
+++ crane_bt_executor/include/crane_bt_executor/skill/kick.hpp
@@ -0,0 +1,44 @@
+// Copyright (c) 2020 ibis-ssl
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+#ifndef CRANE_BT_EXECUTOR__SKILL__KICK_HPP_
+#define CRANE_BT_EXECUTOR__SKILL__KICK_HPP_
+
+#include <iostream>
+#include <memory>
+#include "crane_bt_executor/composite/composite.hpp"
+#include "crane_bt_executor/robot_io.hpp"
+#include "crane_bt_executor/utils/target.hpp"
+
+class Kick : public Composite
+{
+public:
+  Kick(float power = 1.0f)
+  : POWER_(power) {}
+
+  Status run(std::shared_ptr<WorldModel> world_model, RobotIO robot) override
+  {
+    robot.builder->addStraightKick(POWER_);
+
+    return Status::SUCCESS;
+  }
+  const float POWER_;
+};
+#endif  // CRANE_BT_EXECUTOR__SKILL__KICK_HPP_
