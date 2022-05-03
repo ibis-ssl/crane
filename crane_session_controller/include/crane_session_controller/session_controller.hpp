5426af1dee789ec24f279b34211833a578e0325b
Kotaro Yoshimoto <pythagora.yoshimoto@gmail.com>
2022-05-03 19:49:45 +0900

    WIP

--- crane_session_controller/include/crane_session_controller/session_controller.hpp
+++ crane_session_controller/include/crane_session_controller/session_controller.hpp
@@ -24,6 +24,7 @@
 #include <chrono>
 #include <deque>
 #include <memory>
+#include <optional>
 
 #include "crane_msg_wrappers/world_model_wrapper.hpp"
 #include "crane_msgs/msg/world_model.hpp"
@@ -31,74 +32,102 @@
 #include "crane_session_controller/visibility_control.h"
 #include "rclcpp/rclcpp.hpp"
 
-namespace crane
-{
-class SessionModule
-{
-public:
-  SessionModule(std::string name, rclcpp::Node::SharedPtr node) : name(name), node(node) {}
-  void construct()
-  {
-    client = node->create_client<crane_msgs::srv::RobotSelect>("robot_select");
-    using namespace std::chrono_literals;
-    while (!client->wait_for_service(1s)) {
-      if (!rclcpp::ok()) {
-        RCLCPP_ERROR(
-          rclcpp::get_logger(name.c_str()), "Interrupted while waiting for the service. Exiting.");
-        break;
-      }
-      RCLCPP_INFO(rclcpp::get_logger(name.c_str()), "service not available, waiting again...");
-    }
-  }
+namespace crane {
+    class SessionModule {
+    public:
+        using SharedPtr = std::shared_ptr<SessionModule>;
+        SessionModule(std::string name, rclcpp::Node::SharedPtr node) : name(name), node(node) {}
 
-  std::optional<crane_msgs::srv::RobotSelect::Response> sendRequest(
-    crane_msgs::srv::RobotSelect::Request::SharedPtr request)
-  {
-    auto result = client->async_send_request(request);
-    // Wait for the result.
-    if (rclcpp::spin_until_future_complete(node, result) == rclcpp::FutureReturnCode::SUCCESS) {
-      return result.get();
-    } else {
-      return nullopt;
-    }
-  }
+        void construct() {
+            client = node->create_client<crane_msgs::srv::RobotSelect>("robot_select");
+            using namespace std::chrono_literals;
+            while (!client->wait_for_service(1s)) {
+                if (!rclcpp::ok()) {
+                    RCLCPP_ERROR(
+                            rclcpp::get_logger(name.c_str()), "Interrupted while waiting for the service. Exiting.");
+                    break;
+                }
+                RCLCPP_INFO(rclcpp::get_logger(name.c_str()), "service not available, waiting again...");
+            }
+        }
 
-private:
-  rclcpp::Client<crane_msgs::srv::RobotSelect>::SharedPtr client;
-  const std::string name;
-  const rclcpp::Node::SharedPtr node;
-};
+        std::optional<crane_msgs::srv::RobotSelect::Response> sendRequest(
+                crane_msgs::srv::RobotSelect::Request::SharedPtr request) {
+            auto result = client->async_send_request(request);
+            // Wait for the result.
+            if (rclcpp::spin_until_future_complete(node, result) == rclcpp::FutureReturnCode::SUCCESS) {
+                return std::optional<crane_msgs::srv::RobotSelect::Response>(*result.get());
+            } else {
+                return std::nullopt;
+            }
+        }
 
-class SessionContoller : public rclcpp::Node
-{
-public:
-  COMPOSITION_PUBLIC
-  explicit SessionContoller(const rclcpp::NodeOptions & options)
-  : rclcpp::Node("session_controller", options), replace_planner_("replace")
-  {
-    replace_planner_.construct(this);
+    private:
+        rclcpp::Client<crane_msgs::srv::RobotSelect>::SharedPtr client;
+        const std::string name;
+        const rclcpp::Node::SharedPtr node;
+    };
 
-    timer_ = create_wall_timer(1s, std::bind(&SessionContoller::timerCallback, this));
-  }
-  void timerCallback() {}
+    class SessionContoller : public rclcpp::Node {
+    public:
+        COMPOSITION_PUBLIC
+        explicit SessionContoller(const rclcpp::NodeOptions &options)
+                : rclcpp::Node("session_controller", options) {
 
-  void request() {}
+            // example of adding planner
+            session_planners_["replace"]  = std::make_shared<SessionModule>("replace", shared_from_this());
+            for(auto &planner : session_planners_){
+                planner.second->construct();
+            }
 
-private:
-  rclcpp::TimerBase::SharedPtr timer_;
-  rclcpp::Subscription<crane_msgs::msg::WorldModel>::SharedPtr sub_world_model_;
-  std::shared_ptr<WorldModelWrapper> world_model_;
-  std::deque<crane_msgs::srv::RobotSelect::Request> query_queue_;
-  rclcpp::Client<crane_msgs::srv::RobotSelect>::SharedPtr robot_select_client_;
-  SessionModule replace_planner_;
-//  situation > planner ,
-  std::unordered_map<std::string, >
+            // example for ball replacement
+            // TODO : load from config file
+            // TODO:
+            auto replace_map = std::make_shared<std::vector<std::pair<std::string, int>>>();
+            replace_map->emplace_back("goalie",1);
+            replace_map->emplace_back("replace",2);
+            replace_map->emplace_back("waiter",100);
+            robot_selection_priority_map["ball_replacement"] = replace_map;
 
-  void world_model_callback(const crane_msgs::msg::WorldModel::SharedPtr msg)
-  {
-    world_model_->update(*msg);
-  }
-};
+            using namespace std::chrono_literals;
+            timer_ = create_wall_timer(1s, std::bind(&SessionContoller::timerCallback, this));
+        }
+
+        void timerCallback() {}
+        void reassignRequestCallback(){
+            request("replace");
+        }
+
+        void request(std::string situation) {
+            // TODO: reflect actual robot ids
+            std::vector<int> available_robot_ids = {1,2,3,4};
+            auto map = robot_selection_priority_map[situation];
+            if(map){
+                for(auto p : *map){
+                    auto req = std::make_shared<crane_msgs::srv::RobotSelect::Request>();
+                    req->robot_num = p.second;
+                    for(auto id : available_robot_ids){
+                        req->robot_ids.emplace_back(id);
+                    }
+                    session_planners_[p.first]->sendRequest(req);
+                }
+            }
+        }
+
+    private:
+        rclcpp::TimerBase::SharedPtr timer_;
+        rclcpp::Subscription<crane_msgs::msg::WorldModel>::SharedPtr sub_world_model_;
+        std::shared_ptr<WorldModelWrapper> world_model_;
+        std::deque<crane_msgs::srv::RobotSelect::Request> query_queue_;
+        rclcpp::Client<crane_msgs::srv::RobotSelect>::SharedPtr robot_select_client_;
+        std::unordered_map<std::string, SessionModule::SharedPtr> session_planners_;
+
+        std::unordered_map<std::string, std::shared_ptr<std::vector<std::pair<std::string, int>>>> robot_selection_priority_map;
+
+        void world_model_callback(const crane_msgs::msg::WorldModel::SharedPtr msg) {
+            world_model_->update(*msg);
+        }
+    };
 
 }  // namespace crane
 #endif  // CRANE_SESSION_CONTROLLER__SESSION_CONTROLLER_HPP_
