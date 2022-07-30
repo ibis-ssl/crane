5e2eea0a4d90215273c565826b9e9cedc8da59b3
Kotaro Yoshimoto <pythagora.yoshimoto@gmail.com>
2022-07-30 17:28:23 +0900

    WIP

--- session/crane_defender_planner/include/crane_defender_planner/defender_planner.hpp
+++ session/crane_defender_planner/include/crane_defender_planner/defender_planner.hpp
@@ -25,6 +25,8 @@
 #include <memory>
 
 #include "crane_defender_planner/visibility_control.h"
+#include "crane_geometry/boost_geometry.hpp"
+#include "crane_geometry/position_assignments.hpp"
 #include "crane_msg_wrappers/world_model_wrapper.hpp"
 #include "crane_msgs/msg/control_target.hpp"
 #include "crane_msgs/srv/robot_select.hpp"
@@ -46,24 +48,128 @@ public:
   std::vector<crane_msgs::msg::RobotCommand> calculateControlTarget(
     const std::vector<RobotIdentifier> & robots) override
   {
+    std::cout << "calculating control target" << std::endl;
     auto ball = world_model_->ball.pos;
+    Point area_left_bottom, area_left_top, area_right_bottom, area_right_top;
+    const double OFFEST_X = 0.5;
+    const double OFFEST_Y = 0.5;
+    area_left_bottom << -world_model_->field_size.x() * 0.5,
+      world_model_->defense_area.y() * 0.5 + OFFEST_Y;
+    area_left_top = area_left_bottom;
+    area_left_top.x() += world_model_->defense_area.x() + OFFEST_X;
 
+    area_right_bottom << -world_model_->field_size.x() * 0.5,
+      -world_model_->defense_area.y() * 0.5 - OFFEST_Y;
+    area_right_top = area_left_bottom;
+    area_right_top.x() += world_model_->defense_area.x() + OFFEST_X;
 
-    std::vector<crane_msgs::msg::RobotCommand> control_targets;
+    Point goal_center(-world_model_->field_size.x() * 0.5, 0.0);
+
+    std::vector<Segment> segments;
+    segments.emplace_back(area_left_bottom, area_left_top);
+    segments.emplace_back(area_right_bottom, area_right_top);
+    segments.emplace_back(area_left_top, area_right_top);
+
+    //
+    // calc ball line
+    //
+    Segment ball_line(ball, ball + world_model_->ball.vel.normalized() * 20.f);
+    {
+      Point goal_l, goal_r;
+      goal_l << -world_model_->field_size.x() * 0.5f, 0.5f;
+      goal_r << -world_model_->field_size.x() * 0.5f, -0.5f;
+      Segment goal_line(goal_r, goal_l);
+      std::vector<Point> intersections;
+      bg::intersection(ball_line, goal_line, intersections);
+      if (intersections.empty()) {
+        ball_line.first = goal_center;
+        ball_line.second = ball;
+      }
+    }
+
+    //
+    // calculate defense_point
+    //
+    std::vector<Point> intersections;
+    Point defense_point;
+    Segment defense_line;
+    for (auto seg : segments) {
+      bg::intersection(seg, ball_line, intersections);
+      if (not intersections.empty()) {
+        defense_point = intersections.front();
+        defense_line = seg;
+        break;
+      }
+    }
+
+    std::cout << "end calculate defense_point " << std::endl;
+
+    //
+    // list up defense points
+    //
+    const double DEFENSE_INTERVAL = 0.5;
+    std::vector<Point> defense_points;
+    Point diff_defense_line = defense_line.first - defense_line.second;
+    if (diff_defense_line.x() = 0.) {
+      // front defense
+      if (robots.size() % 2 == 0) {
+        defense_point.y() -= DEFENSE_INTERVAL * 0.5;
+      }
+      defense_points.push_back(defense_point);
+      for (int i = 0; i < robots.size() - 1; i++) {
+        Point p = defense_point;
+        if (i % 2 == 0) {
+          p.y() += DEFENSE_INTERVAL * (i / 2 + 1);
+        } else {
+          p.y() -= DEFENSE_INTERVAL * (i / 2 + 1);
+        }
+        defense_points.push_back(p);
+      }
+    } else {
+      // side defense
+      if (robots.size() % 2 == 0) {
+        defense_point.x() -= DEFENSE_INTERVAL * 0.5;
+      }
+      defense_points.push_back(defense_point);
+      for (int i = 0; i < robots.size() - 1; i++) {
+        Point p = defense_point;
+        if (i % 2 == 0) {
+          p.x() += DEFENSE_INTERVAL * (i / 2 + 1);
+        } else {
+          p.x() -= DEFENSE_INTERVAL * (i / 2 + 1);
+        }
+        defense_points.push_back(p);
+      }
+    }
+
+    std::cout << "end list up defense_point " << std::endl;
+    std::vector<Point> robot_points;
     for (auto robot_id : robots) {
+      robot_points.emplace_back(world_model_->getRobot(robot_id)->pose.pos);
+    }
+
+    std::cout << "solve start" << std::endl;
+    auto solution = getOptimalAssignments(robot_points, defense_points);
+
+    std::cout << "solve end" << std::endl;
+    std::vector<crane_msgs::msg::RobotCommand> control_targets;
+    for (auto robot_id = robots.begin(); robot_id != robots.end(); ++robot_id) {
+      int index = std::distance(robots.begin(), robot_id);
+      Point target_point = defense_points[index];
+
       crane_msgs::msg::RobotCommand target;
-      auto robot = world_model_->getRobot(robot_id);
+      auto robot = world_model_->getRobot(*robot_id);
       // Stop at same position
-      target.robot_id = robot_id.robot_id;
+      target.robot_id = robot_id->robot_id;
       target.chip_enable = false;
       target.dribble_power = 0.0;
       target.kick_power = 0.0;
-      // control by velocity
-      target.motion_mode_enable = true;
+      // control by position
+      target.motion_mode_enable = false;
       // Stop at same position
-      target.target.x = 0.0;      // vx
-      target.target.y = 0.0;      // vy
-      target.target.theta = 0.0;  // omega
+      target.target.x = target_point.x();  // vx
+      target.target.y = target_point.y();  // vy
+      target.target.theta = 0.0;           // omega
       control_targets.emplace_back(target);
     }
     return control_targets;
