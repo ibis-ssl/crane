604ce5acbd3d8651791a0ae05760d4e76d8f567d
Kotaro Yoshimoto <pythagora.yoshimoto@gmail.com>
2022-08-06 05:30:08 +0900

    Implement structure of BallPlacementPlanner
    Signed-off-by: Kotaro Yoshimoto <pythagora.yoshimoto@gmail.com>

--- session/crane_ball_placement_planner/include/crane_ball_placement_planner/ball_placement_planner.hpp
+++ session/crane_ball_placement_planner/include/crane_ball_placement_planner/ball_placement_planner.hpp
@@ -21,6 +21,7 @@
 #ifndef CRANE_BALL_PLACEMENT_PLANNER__BALL_PLACEMENT_PLANNER_HPP_
 #define CRANE_BALL_PLACEMENT_PLANNER__BALL_PLACEMENT_PLANNER_HPP_
 
+#include <boost/range/adaptor/indexed.hpp>
 #include <functional>
 #include <memory>
 
@@ -34,28 +35,171 @@
 
 namespace crane
 {
+enum class BallPlacementState {
+  START,
+  WALL_KICK_PREPARE,
+  WALL_KICK_GO,
+  PLACE_PREPARE,
+  PLACE_KICK_GO,
+  PLACE_DRIBBLE_GO,
+  FINISH,
+};
 class BallPlacementPlannerComponent : public rclcpp::Node, public PlannerBase
 {
 public:
   COMPOSITION_PUBLIC
   explicit BallPlacementPlannerComponent(const rclcpp::NodeOptions & options)
-  : rclcpp::Node("template_planner", options), PlannerBase("template", *this)
+  : rclcpp::Node("ball_placement_planner", options), PlannerBase("ball_placement", *this)
   {
     RCLCPP_INFO(get_logger(), "initializing");
+    addRobotSelectCallback([&]() { state_ = BallPlacementState::START; });
+  }
+
+  bool isDribbleMode(const std::vector<RobotIdentifier> & robots) const
+  {
+    if (robots.size() == 1) {
+      return true;
+    } else if (world_model_->getDistanceFromRobotToBall(robots.front()) <= 1.0) {
+      return true;
+    }
+    return false;
+  }
+
+  bool isWallKickRequired()
+  {
+    auto ball = world_model_->ball.pos;
+    constexpr double OFFSET = 0.2;
+    if (abs(ball.x()) > (world_model_->field_size.x() * 0.5 - OFFSET)) {
+      return true;
+    }
+    if (abs(ball.y()) > (world_model_->field_size.y() * 0.5 - OFFSET)) {
+      return true;
+    }
+    return false;
+  }
+
+  static constexpr double PREPARE_THRESHOLD = 0.1;
+  void executeWallKickPrepare(
+    const std::vector<RobotIdentifier> & robots,
+    std::vector<crane_msgs::msg::RobotCommand> & control_targets)
+  {
+    Point prepare_point;  // TODO: calculate prepare_point
+    crane_msgs::msg::RobotCommand target;
+    target.robot_id = robots.front().robot_id;
+    target.chip_enable = false;
+    target.kick_power = 0.5;
+    target.dribble_power = 0.0;
+    target.motion_mode_enable = false;
+    target.target.x = prepare_point.x();
+    target.target.y = prepare_point.y();
+    control_targets.emplace_back(target);
+    if (
+      world_model_->getDistanceFromRobot({true, target.robot_id}, prepare_point) <
+      PREPARE_THRESHOLD) {
+      state_ = BallPlacementState::WALL_KICK_GO;
+    }
+  }
+  void executeWallKickGo(
+    const std::vector<RobotIdentifier> & robots,
+    std::vector<crane_msgs::msg::RobotCommand> & control_targets)
+  {
+    crane_msgs::msg::RobotCommand target;
+    auto robot = world_model_->getRobot(robots.front());
+    target.robot_id = robot->id;
+    target.chip_enable = false;
+    target.kick_power = 0.5;
+    target.dribble_power = 0.0;
+    target.motion_mode_enable = true;
+
+    auto vel = (world_model_->ball.pos - robot->pose.pos).normalized() * 0.5;
+    target.target.x = vel.x();
+    target.target.y = vel.y();
+    target.target.theta = getAngle(vel);
+    control_targets.emplace_back(target);
+  }
+  void executePlacePrepare(
+    const std::vector<RobotIdentifier> & robots,
+    std::vector<crane_msgs::msg::RobotCommand> & control_targets)
+  {
+  }
+  void executePlaceKickGo(
+    const std::vector<RobotIdentifier> & robots,
+    std::vector<crane_msgs::msg::RobotCommand> & control_targets)
+  {
+  }
+  void executePlaceDribbleGo(
+    const std::vector<RobotIdentifier> & robots,
+    std::vector<crane_msgs::msg::RobotCommand> & control_targets)
+  {
+  }
+  void executeFinish(
+    const std::vector<RobotIdentifier> & robots,
+    std::vector<crane_msgs::msg::RobotCommand> & control_targets)
+  {
   }
 
   std::vector<crane_msgs::msg::RobotCommand> calculateControlTarget(
     const std::vector<RobotIdentifier> & robots) override
   {
     std::vector<crane_msgs::msg::RobotCommand> control_targets;
-    for (auto robot_id : robots) {
-      crane_msgs::msg::RobotCommand target;
-      auto robot = world_model_->getRobot(robot_id);
+    switch (state_) {
+      case BallPlacementState::START: {
+        state_ = (isWallKickRequired()) ? BallPlacementState::WALL_KICK_PREPARE
+                                        : BallPlacementState::PLACE_PREPARE;
+        calculateControlTarget(robots);
+        break;
+      }
+      case BallPlacementState::WALL_KICK_PREPARE: {
+        executeWallKickPrepare(robots, control_targets);
+        break;
+      }
+      case BallPlacementState::WALL_KICK_GO: {
+        executeWallKickGo(robots, control_targets);
+        break;
+      }
+      case BallPlacementState::PLACE_PREPARE: {
+        executePlacePrepare(robots, control_targets);
+        break;
+      }
+      case BallPlacementState::PLACE_KICK_GO: {
+        executePlaceKickGo(robots, control_targets);
+        break;
+      }
+      case BallPlacementState::PLACE_DRIBBLE_GO: {
+        executePlaceDribbleGo(robots, control_targets);
+        break;
+      }
+      case BallPlacementState::FINISH: {
+        executeFinish(robots, control_targets);
+        break;
+      }
+    }
 
-      target.robot_id = robot_id.robot_id;
+    if (required_wall_kick) {
+      crane_msgs::msg::RobotCommand target;
+      target.robot_id = robots.front().robot_id;
+      target.chip_enable = false;
+      target.kick_power = 0.5;
+      target.dribble_power = 0.0;
+      return control_targets;
+    }
+
+    for (auto r : robots | boost::adaptors::indexed()) {
+      crane_msgs::msg::RobotCommand target;
+      auto robot = world_model_->getRobot(r.value());
+      // common param
+      target.disable_placement_avoidance = true;
+      target.robot_id = robot->id;
       target.chip_enable = false;
       target.dribble_power = 0.0;
       target.kick_power = 0.0;
+
+      if (dribble_mode) {
+        if (r.index() > 0) {
+          target.disable_placement_avoidance = false;
+        } else {
+        }
+      }
 
       // TODO: implement
       target.motion_mode_enable = false;
@@ -70,8 +214,11 @@ public:
   double getRoleScore(std::shared_ptr<RobotInfo> robot) override
   {
     // the nearest to the ball first
-    return 100. / (robot->pose.pos - world_model_->ball.pos).squaredNorm();
+    return 100. / world_model_->getSquareDistanceFromRobotToBall({true, robot->id});
   }
+
+private:
+  BallPlacementState state_;
 };
 
 }  // namespace crane
