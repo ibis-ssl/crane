610bfabf6f672f057c55db9ddd7a9e8983129b45
Kotaro Yoshimoto <pythagora.yoshimoto@gmail.com>
2023-03-25 14:54:12 +0900

    sim_sender: 角度制御にPID制御を導入

--- crane_sender/include/crane_sender/sim_sender.hpp
+++ crane_sender/include/crane_sender/sim_sender.hpp
@@ -20,6 +20,34 @@
 
 namespace crane
 {
+
+class PIDController
+{
+public:
+  PIDController() = default;
+  void setGain(float kp, float ki, float kd)
+  {
+    kp_ = kp;
+    ki_ = ki;
+    kd_ = kd;
+    error_prev_ = 0.0f;
+  }
+  float update(float error, float dt)
+  {
+    float p = kp_ * error;
+    float i = ki_ * (error + error_prev_) * dt / 2.0f;
+    float d = kd_ * (error - error_prev_) / dt;
+    error_prev_ = error;
+    return p + i + d;
+  }
+
+private:
+  float kp_;
+  float ki_;
+  float kd_;
+  float error_prev_;
+};
+
 class SimSenderComponent : public rclcpp::Node
 {
 public:
@@ -32,6 +60,10 @@ public:
     //    sub_replacement_ = this->create_subscription<robocup_ssl_msgs::msg::Replacement>(
     //      "sim_sender/replacements", 10, std::bind(&SimSender::send_replacement, this, std::placeholders::_1));
     pub_commands_ = this->create_publisher<robocup_ssl_msgs::msg::Commands>("/commands", 10);
+
+    for (auto & controller : theta_controllers) {
+      controller.setGain(4, 0.00, 0.1);
+    }
   }
 
   //private:
@@ -61,7 +93,7 @@ public:
     }
   }
 
-  void send_commands(const crane_msgs::msg::RobotCommands::SharedPtr msg) const
+  void send_commands(const crane_msgs::msg::RobotCommands::SharedPtr msg)
   {
     const double MAX_KICK_SPEED = 8.0;  // m/s
     robocup_ssl_msgs::msg::Commands commands;
@@ -76,8 +108,9 @@ public:
       cmd.set__veltangent(command.target.x);
       cmd.set__velnormal(command.target.y);
 
-      float diff = getAngleDiff(command.current_theta, command.target.theta);
-      cmd.set__velangular(-10.0 * diff);
+      float omega = theta_controllers.at(command.robot_id)
+                      .update(getAngleDiff(command.current_theta, command.target.theta), 0.033);
+      cmd.set__velangular(omega);
 
       // キック速度
       double kick_speed = command.kick_power * MAX_KICK_SPEED;
@@ -136,6 +169,7 @@ public:
   //  rclcpp::Subscription<consai2r2_msgs::msg::Replacements>::SharedPtr sub_replacement_;
   rclcpp::Publisher<robocup_ssl_msgs::msg::Commands>::SharedPtr pub_commands_;
   std::array<float, 11> vel;
+  std::array<PIDController, 11> theta_controllers;
 };
 
 }  // namespace crane
