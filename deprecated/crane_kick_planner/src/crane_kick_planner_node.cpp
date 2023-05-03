a81b46b84436cc6efde9f314eb16dff709990f53
Kotaro Yoshimoto <pythagora.yoshimoto@gmail.com>
2023-05-03 16:37:57 +0900

    雑務：deprecatedフォルダの中身を削除

--- deprecated/crane_kick_planner/src/crane_kick_planner_node.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2022 ibis-ssl
-//
-// Use of this source code is governed by an MIT-style
-// license that can be found in the LICENSE file or at
-// https://opensource.org/licenses/MIT.
-
-#include <iostream>
-
-int main() { return 0; }
