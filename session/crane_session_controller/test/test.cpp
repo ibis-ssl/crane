4b100b5e837443a1d2e6a5d8859c50376c926eed
Kotaro Yoshimoto <pythagora.yoshimoto@gmail.com>
2023-05-13 20:36:40 +0900

    整備：テスト周りの整備

---
+++ session/crane_session_controller/test/test.cpp
@@ -0,0 +1,14 @@
+// Copyright (c) 2023 ibis-ssl
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+#include "crane_session_controller/session_controller.hpp"
+#include <gtest/gtest.h>
+
+
+TEST(SessionController, test)
+{
+  ASSERT_NEAR(1, 1, 1e-5);
+}
