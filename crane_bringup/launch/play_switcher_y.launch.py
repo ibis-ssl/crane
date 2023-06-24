b27d14922506773042c226f5e9766750d1bf11d2
Kotaro Yoshimoto <pythagora.yoshimoto@gmail.com>
2023-06-25 08:29:52 +0900

    途中

---
+++ crane_bringup/launch/play_switcher_y.launch.py
@@ -0,0 +1,179 @@
+# Copyright (c) 2022 ibis-ssl
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    declare_arg_vision_addr = DeclareLaunchArgument(
+        "vision_addr",
+        default_value="224.5.23.2",
+        description="Set multicast address to connect SSL-Vision.",
+    )
+
+    declare_arg_vision_port = DeclareLaunchArgument(
+        "vision_port",
+        default_value="10006",
+        description="Set multicast port to connect SSL-Vision.",
+    )
+
+    declare_arg_referee_addr = DeclareLaunchArgument(
+        "referee_addr",
+        default_value="224.5.23.1",
+        description="Set multicast address to connect Game Controller.",
+    )
+
+    declare_arg_referee_port = DeclareLaunchArgument(
+        "referee_port",
+        default_value="11003",
+        description="Set multicast port to connect Game Controller.",
+    )
+
+    session_controller = Node(
+        package="crane_session_controller",
+        executable="crane_session_controller_node",
+        output="screen",
+        parameters=[
+            {
+                # "initial_session": "OUR_KICKOFF_START",
+                # "initial_session": "goalie",
+                "initial_session": "defense",
+            }
+        ],
+    )
+
+    local_planner = Node(
+        package="crane_local_planner",
+        executable="crane_local_planner_node",
+        output="screen",
+        parameters=[
+            {
+                "enable_rvo": False,
+            }
+        ],
+    )
+
+    waiter = Node(package="crane_planner_plugins", executable="waiter_node")
+
+    goalie = Node(
+        package="crane_planner_plugins",
+        executable="goalie_node",
+        # output="screen"
+    )
+
+    attacker = Node(
+        package="crane_planner_plugins",
+        executable="attacker_node",
+        output="screen"
+    )
+
+    defender = Node(
+        package="crane_planner_plugins",
+        executable="defender_node",
+        output="screen"
+    )
+
+    kickoff = Node(package="crane_planner_plugins", executable="kickoff_node", output="screen")
+
+    vision = Node(
+        package="robocup_ssl_comm",
+        executable="vision_node",
+        parameters=[
+            {
+                "multicast_address": LaunchConfiguration("vision_addr"),
+                "multicast_port": LaunchConfiguration("vision_port"),
+            }
+        ],
+    )
+
+    game_controller = Node(
+        package="robocup_ssl_comm",
+        executable="game_controller_node",
+        parameters=[
+            {
+                "multicast_address": LaunchConfiguration("referee_addr"),
+                "multicast_port": LaunchConfiguration("referee_port"),
+            }
+        ],
+    )
+
+    grsim = Node(package="robocup_ssl_comm", executable="grsim_node")
+
+    vision_tracker = Node(package="consai_vision_tracker", executable="vision_tracker_node")
+
+    world_model_publisher = Node(
+        package="crane_world_model_publisher",
+        executable="crane_world_model_publisher_node",
+        # output="screen",
+        parameters=[
+            {
+                "initial_team_color": "YELLOW",
+            }
+        ],
+    )
+
+    play_switcher = Node(
+        package="crane_play_switcher", executable="play_switcher_node", output="screen"
+    )
+
+    visualizer = Node(package="consai_visualizer", executable="consai_visualizer", output="screen")
+
+    sim_sender = Node(
+        package="crane_sender",
+        executable="sim_sender_node",
+        output="screen",
+        parameters=[
+            {
+                "no_movement": False,
+                "theta_kp": 3.7,
+                "theta_ki": 0.0,
+                "theta_kd": 0.5,
+            }
+        ],
+    )
+
+    real_sender = Node(
+        package="crane_sender",
+        executable="real_sender_node",
+        # output="screen",
+        parameters=[{}],
+    )
+
+    return LaunchDescription(
+        [
+            declare_arg_vision_addr,
+            declare_arg_vision_port,
+            declare_arg_referee_addr,
+            declare_arg_referee_port,
+            vision,
+            game_controller,
+            grsim,
+            vision_tracker,
+            session_controller,
+            local_planner,
+            # real_sender,
+            sim_sender,
+            defender,
+            waiter,
+            goalie,
+            kickoff,
+            attacker,
+            world_model_publisher,
+            play_switcher,
+            visualizer,
+        ]
+    )
--- session/crane_session_controller/include/crane_session_controller/session_module.hpp
+++ session/crane_session_controller/include/crane_session_controller/session_module.hpp
@@ -23,7 +23,8 @@ public:
 
   void construct(rclcpp::Node & node)
   {
-    std::string service_name = "/session/" + name + "/robot_select";
+    node_base = node.get_node_base_interface();
+    std::string service_name = "/session/" + name + "/assign";
     client = node.create_client<crane_msgs::srv::RobotSelect>(service_name);
     using namespace std::chrono_literals;
     while (!client->wait_for_service(1s)) {
@@ -39,14 +40,14 @@ public:
     RCLCPP_INFO(rclcpp::get_logger(service_name.c_str()), "service connected!");
   }
 
-  std::optional<crane_msgs::srv::RobotSelect::Response> sendRequest(
-    crane_msgs::srv::RobotSelect::Request::SharedPtr request, rclcpp::Node & node)
+  std::optional<crane_msgs::srv::RobotSelect::Response> assign(
+    crane_msgs::srv::RobotSelect::Request::SharedPtr request)
   {
-    std::cout << "Sending request to " << name << std::endl;
+    std::cout << "Assigning robots to " << name << std::endl;
     auto result_future = client->async_send_request(request);
     // Wait for the result.
     if (
-      rclcpp::spin_until_future_complete(node.get_node_base_interface(), result_future) ==
+      rclcpp::spin_until_future_complete(node_base, result_future) ==
       rclcpp::FutureReturnCode::SUCCESS) {
       std::cout << "Received response from " << name << std::endl;
       return *result_future.get();
@@ -56,10 +57,16 @@ public:
     }
   }
 
+  void observe(){}
+
 private:
+  rclcpp::node_interfaces::NodeBaseInterface::SharedPtr node_base = nullptr;
+
   rclcpp::Client<crane_msgs::srv::RobotSelect>::SharedPtr client;
 
   const std::string name;
+
+  std::vector<int> assigned_robots;
 };
 }  // namespace crane
 #endif  // CRANE_SESSION_CONTROLLER__SESSION_MODULE_HPP_
