425a5e287dac91b05dc54f4731c7c73a7aaaa5ad
Kotaro Yoshimoto <pythagora.yoshimoto@gmail.com>
2023-07-29 21:42:59 +0900

    頻出関数を geometry_operations.hpp に追加

--- utility/crane_geometry/include/crane_geometry/geometry_operations.hpp
+++ utility/crane_geometry/include/crane_geometry/geometry_operations.hpp
@@ -23,6 +23,31 @@ inline bool isInRect(const Rect & rect, const Point & p)
 
 inline double getAngle(const Vector2 & vec) { return atan2(vec.y(), vec.x()); }
 
+inline float normalizeAngle(float angle_rad)
+{
+  while (angle_rad > M_PI) {
+    angle_rad -= 2.0f * M_PI;
+  }
+  while (angle_rad < -M_PI) {
+    angle_rad += 2.0f * M_PI;
+  }
+  return angle_rad;
+}
+
+inline float getAngleDiff(float angle_rad1, float angle_rad2)
+{
+  angle_rad1 = normalizeAngle(angle_rad1);
+  angle_rad2 = normalizeAngle(angle_rad2);
+  if (abs(angle_rad1 - angle_rad2) > M_PI) {
+    if (angle_rad1 - angle_rad2 > 0) {
+      return angle_rad1 - angle_rad2 - 2.0f * M_PI;
+    } else {
+      return angle_rad1 - angle_rad2 + 2.0f * M_PI;
+    }
+  } else {
+    return angle_rad1 - angle_rad2;
+  }
+}
 }  // namespace crane
 
 #endif  // CRANE_GEOMETRY__GEOMETRY_OPERATIONS_HPP_
