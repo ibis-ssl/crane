a060c7ed1f2260c3edbcea6b1f4fa0ad6dcefd32
Kotaro Yoshimoto <pythagora.yoshimoto@gmail.com>
2023-08-28 22:38:44 +0900

    デフォルトで4台に

--- session/crane_session_controller/src/crane_session_controller.cpp
+++ session/crane_session_controller/src/crane_session_controller.cpp
@@ -104,7 +104,7 @@ SessionControllerComponent::SessionControllerComponent(const rclcpp::NodeOptions
           "イベント「%s」に対応するセッション「%s」の設定に従ってロボットを割り当てます",
           it->first.c_str(), it->second.c_str());
         // TODO: 選択可能なロボットを引っ張ってくる
-        request(it->second, {0, 1});
+        request(it->second, {0, 1, 2, 3});
       } else {
         RCLCPP_ERROR(
           get_logger(), "イベント「%s」に対応するセッションの設定が見つかりませんでした",
