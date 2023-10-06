1b04be8c98f307449b017c1acf12df48096667af
Kotaro Yoshimoto <pythagora.yoshimoto@gmail.com>
2023-10-07 02:54:24 +0900

    アタッカーのキックを強く

--- session/crane_planner_plugins/include/crane_planner_plugins/attacker_planner.hpp
+++ session/crane_planner_plugins/include/crane_planner_plugins/attacker_planner.hpp
@@ -72,7 +72,7 @@ public:
         set_target(target.target_x, world_model->ball.pos.x());
         set_target(target.target_y, world_model->ball.pos.y());
         target.dribble_power = 0.5;
-        target.kick_power = 0.5;
+        target.kick_power = 0.8;
         target.chip_enable = false;
         target.local_planner_config.disable_collision_avoidance = true;
       }
