6ce30f04569b1801841cde2a9b74de6e38adfb0d
Kotaro Yoshimoto <pythagora.yoshimoto@gmail.com>
2023-10-16 21:53:12 +0900

    コミット漏れ

---
+++ crane_robot_skills/src/robot_skills_node.cpp
@@ -0,0 +1,18 @@
+// Copyright (c) 2023 ibis-ssl
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+#include <rclcpp/rclcpp.hpp>
+
+#include "crane_world_model_publisher/world_model_publisher.hpp"
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+  auto node = std::make_shared<crane::WorldModelPublisherComponent>(rclcpp::NodeOptions());
+  rclcpp::spin(node);
+  rclcpp::shutdown();
+  return 0;
+}
