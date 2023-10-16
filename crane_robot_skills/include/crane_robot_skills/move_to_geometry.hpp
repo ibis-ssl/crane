4bad73ca1f6131216eceb633c8dc360c9cdb3656
Kotaro Yoshimoto <pythagora.yoshimoto@gmail.com>
2023-10-16 21:52:49 +0900

    MoveToGeometryクラスを一般化

--- crane_robot_skills/include/crane_robot_skills/move_to_geometry.hpp
+++ crane_robot_skills/include/crane_robot_skills/move_to_geometry.hpp
@@ -4,26 +4,30 @@
 // license that can be found in the LICENSE file or at
 // https://opensource.org/licenses/MIT.
 
-#ifndef CRANE_PLANNER_PLUGINS__MOVE_TO_GEOMETRY_HPP
-#define CRANE_PLANNER_PLUGINS__MOVE_TO_GEOMETRY_HPP
+#ifndef CRANE_ROBOT_SKILLS__MOVE_TO_GEOMETRY_HPP_
+#define CRANE_ROBOT_SKILLS__MOVE_TO_GEOMETRY_HPP_
 
+#include <crane_geometry/eigen_adapter.hpp>
 #include <crane_robot_skills/skill_base.hpp>
 
 namespace crane
 {
+template <typename Geometry>
 class MoveToGeometry : public SkillBase<>
 {
 public:
-  explicit MoveToGeometry(uint8_t id, std::shared_ptr<WorldModelWrapper> & world_model)
-  : SkillBase<>("move_to_geometry", id, world_model, DefaultStates::DEFAULT)
+  explicit MoveToGeometry(
+    uint8_t id, Geometry geometry, std::shared_ptr<WorldModelWrapper> & world_model,
+    const double threshold = 0.1)
+  : SkillBase<>("move_to_geometry", id, world_model, DefaultStates::DEFAULT), geometry(geometry)
   {
     addStateFunction(
       DefaultStates::DEFAULT,
-      [this](
+      [this, threshold](
         const std::shared_ptr<WorldModelWrapper> & world_model,
         const std::shared_ptr<RobotInfo> & robot,
         crane::RobotCommandWrapper & command) -> SkillBase::Status {
-        if ((robot->pose.pos - getTargetPoint()).norm() < 0.1) {
+        if ((robot->pose.pos - getTargetPoint()).norm() < threshold) {
           return SkillBase::Status::SUCCESS;
         } else {
           command.setTargetPosition(getTargetPoint());
@@ -32,7 +36,15 @@ public:
       });
   }
 
-  Point getTargetPoint() { return Point(); }
+  Point getTargetPoint()
+  {
+    ClosestPoint result;
+    bg::closest_point(geometry, robot->pose.pos, result);
+    return result.closest_point;
+  }
+
+protected:
+  Geometry geometry;
 };
 }  // namespace crane
-#endif  //CRANE_PLANNER_PLUGINS__MOVE_TO_GEOMETRY_HPP
+#endif  // CRANE_ROBOT_SKILLS__MOVE_TO_GEOMETRY_HPP_
