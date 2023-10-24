65f28527fffc4cae8db01062842031ee858b0286
Kotaro Yoshimoto <pythagora.yoshimoto@gmail.com>
2023-10-24 23:17:51 +0900

    MoveWithBallSkillを実装

--- crane_robot_skills/include/crane_robot_skills/move_with_ball.hpp
+++ crane_robot_skills/include/crane_robot_skills/move_with_ball.hpp
@@ -12,11 +12,15 @@
 
 namespace crane
 {
+/**
+ * ボールを持って移動する
+ * 前提：GetBallContactが成功していること
+ */
 class MoveWithBall : public SkillBase<>
 {
 public:
-  explicit MoveWithBall(uint8_t id, std::shared_ptr<WorldModelWrapper> & world_model)
-  : SkillBase<>("move_with_ball", id, world_model, DefaultStates::DEFAULT)
+  explicit MoveWithBall(Pose2D pose, uint8_t id, std::shared_ptr<WorldModelWrapper> & world_model)
+  : SkillBase<>("move_with_ball", id, world_model, DefaultStates::DEFAULT), target_pose(pose)
   {
     addStateFunction(
       DefaultStates::DEFAULT,
@@ -24,9 +28,49 @@ public:
         const std::shared_ptr<WorldModelWrapper> & world_model,
         const std::shared_ptr<RobotInfo> & robot,
         crane::RobotCommandWrapper & command) -> SkillBase::Status {
-        return SkillBase::Status::RUNNING;
+        if (not robot->ball_contact.findPastContact(2.0)) {
+          // ボールが離れたら失敗
+          return SkillBase::Status::FAILURE;
+        } else if (
+          (robot->pose.pos).norm() < 0.1 &&
+          std::abs(getAngleDiff(robot->pose.theta, target_pose.theta)) < 0.1) {
+          // ターゲットに到着したら成功
+          return SkillBase::Status::SUCCESS;
+        } else {
+          command.setTargetPosition(getTargetPoint());
+          command.setTargetTheta(getTargetAngle());
+          command.dribble(0.5);
+          return SkillBase::Status::RUNNING;
+        }
       });
   }
+
+  Point getTargetPoint()
+  {
+    // 正しい方向でドリブルできている場合だけ前進
+    if (getAngleDiff(robot->pose.theta, getTargetAngle()) < 0.3) {
+      if (robot->ball_contact.findPastContact(0.5)) {
+        return robot->pose.pos;
+      }
+    }
+  }
+
+  double getTargetAngle()
+  {
+    auto distance = world_model->getDistanceFromRobot(robot->getID(), target_pose.pos);
+    auto to_target = getAngle(target_pose.pos - robot->pose.pos);
+
+    if (distance > 0.2) {
+      return to_target;
+    } else if (distance > 0.1) {
+      double ratio = (distance - 0.1) / (0.2 - 0.1);
+      return ratio * to_target + (1.0 - ratio) * target_pose.theta;
+    } else {
+      return target_pose.theta;
+    }
+  }
+
+  Pose2D target_pose;
 };
 }  // namespace crane
 #endif  // CRANE_ROBOT_SKILLS__MOVE_WITH_BALL_HPP_
