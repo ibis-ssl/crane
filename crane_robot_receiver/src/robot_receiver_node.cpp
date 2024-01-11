db5fdb9633ebbcd2f93187c740617f60d8196621
Kotaro Yoshimoto <pythagora.yoshimoto@gmail.com>
2024-01-11 22:13:52 +0900

    GUIから移植

--- crane_robot_receiver/src/robot_receiver_node.cpp
+++ crane_robot_receiver/src/robot_receiver_node.cpp
@@ -4,6 +4,8 @@
 // license that can be found in the LICENSE file or at
 // https://opensource.org/licenses/MIT.
 
+#include <atomic>
+
 #include <boost/asio.hpp>
 #include <boost/thread.hpp>
 #include <crane_msgs/msg/robot_feed_back.hpp>
@@ -27,6 +29,33 @@ auto makeConfig(uint8_t id)
   return config;
 }
 
+struct RobotFeedback
+{
+  uint8_t head[2];
+  uint8_t counter, return_counter;
+
+  uint8_t kick_state;
+
+  uint8_t temperature[7];
+  uint8_t error_info[8];
+  int8_t motor_current[4];
+  uint8_t ball_detection[4];
+
+  float_t yaw_angle, diff_angle;
+  float_t odom[2], odom_speed[2], mouse_raw[2], voltage[2];
+};
+
+static constexpr int TX_BUF_SIZE_ETHER = 128;
+typedef union {
+  uint8_t buf[TX_BUF_SIZE_ETHER];
+  RobotFeedback data;
+} RobotInfoUnion;
+
+union FloatUnion {
+  float f;
+  char b[4];
+};
+
 struct RobotInterface
 {
   RobotInterface(const RobotInterfaceConfig config)
@@ -44,24 +73,136 @@ struct RobotInterface
     socket.set_option(boost::asio::ip::multicast::join_group(address));
 
     socket.async_receive_from(
-      boost::asio::buffer(data_, max_length), sender_endpoint,
+      boost::asio::buffer(buffer, max_length), sender_endpoint,
       std::bind(&RobotInterface::handle_receive, this, _1, _2));
   }
 
-  void handle_receive(const boost::system::error_code & error, size_t bytes_recvd)
+  void handle_receive(const boost::system::error_code & error, size_t bytes)
   {
-    if (!error) {
-      // receive data
-      //      std::string message(data_, bytes_recvd);
+    if (error) {
+      return;
     }
+
+    FloatUnion float_union;
+    RobotInfoUnion robot_info_union;
+    robot_info_union.data = robot_feedback.load();
+
+    // 0,1byte目は識別子みたいな感じ
+    auto header = buffer[2];
+    switch (header) {
+      case 10: {
+        robot_info_union.data.counter = buffer[3];
+        {
+          float_union.b[0] = buffer[4];
+          float_union.b[1] = buffer[5];
+          float_union.b[2] = buffer[6];
+          float_union.b[3] = buffer[7];
+          robot_info_union.data.yaw_angle = float_union.f;
+        }
+        {
+          float_union.b[0] = buffer[8];
+          float_union.b[1] = buffer[9];
+          float_union.b[2] = buffer[10];
+          float_union.b[3] = buffer[11];
+          robot_info_union.data.diff_angle = float_union.f;
+        }
+        robot_info_union.data.ball_detection[0] = buffer[12];
+        robot_info_union.data.ball_detection[1] = buffer[13];
+        robot_info_union.data.ball_detection[2] = buffer[14];
+        robot_info_union.data.ball_detection[3] = buffer[15];
+      } break;
+      case 11: {
+        robot_info_union.data.error_info[0] = buffer[4];
+        robot_info_union.data.error_info[1] = buffer[5];
+        robot_info_union.data.error_info[2] = buffer[6];
+        robot_info_union.data.error_info[3] = buffer[7];
+        robot_info_union.data.error_info[4] = buffer[8];
+        robot_info_union.data.error_info[5] = buffer[9];
+        robot_info_union.data.error_info[6] = buffer[10];
+        robot_info_union.data.error_info[7] = buffer[11];
+        robot_info_union.data.motor_current[0] = buffer[12];
+        robot_info_union.data.motor_current[1] = buffer[13];
+        robot_info_union.data.motor_current[2] = buffer[14];
+        robot_info_union.data.motor_current[3] = buffer[15];
+      } break;
+      case 12: {
+        robot_info_union.data.kick_state = buffer[4] * 10;
+        robot_info_union.data.temperature[0] = buffer[5];
+        robot_info_union.data.temperature[1] = buffer[6];
+        robot_info_union.data.temperature[2] = buffer[7];
+        robot_info_union.data.temperature[3] = buffer[8];
+        robot_info_union.data.temperature[4] = buffer[9];
+        robot_info_union.data.temperature[5] = buffer[10];
+        robot_info_union.data.temperature[6] = buffer[11];
+        {
+          float_union.b[0] = buffer[12];
+          float_union.b[1] = buffer[13];
+          float_union.b[2] = buffer[14];
+          float_union.b[3] = buffer[15];
+          robot_info_union.data.voltage[0] = float_union.f;
+        }
+      } break;
+      case 13: {
+        {
+          float_union.b[0] = buffer[4];
+          float_union.b[1] = buffer[5];
+          float_union.b[2] = buffer[6];
+          float_union.b[3] = buffer[7];
+          robot_info_union.data.voltage[1] = float_union.f;
+        }
+        {
+          float_union.b[0] = buffer[8];
+          float_union.b[1] = buffer[9];
+          float_union.b[2] = buffer[10];
+          float_union.b[3] = buffer[11];
+          robot_info_union.data.odom[0] = float_union.f * 1000;
+        }
+        {
+          float_union.b[0] = buffer[12];
+          float_union.b[1] = buffer[13];
+          float_union.b[2] = buffer[14];
+          float_union.b[3] = buffer[15];
+          robot_info_union.data.odom[1] = float_union.f * 1000;
+        }
+      } break;
+      case 14: {
+        robot_info_union.data.return_counter = buffer[3];
+        {
+          float_union.b[0] = buffer[4];
+          float_union.b[1] = buffer[5];
+          float_union.b[2] = buffer[6];
+          float_union.b[3] = buffer[7];
+          robot_info_union.data.odom_speed[0] = float_union.f;
+        }
+        {
+          float_union.b[0] = buffer[8];
+          float_union.b[1] = buffer[9];
+          float_union.b[2] = buffer[10];
+          float_union.b[3] = buffer[11];
+          robot_info_union.data.odom_speed[1] = float_union.f;
+        }
+      } break;
+      default:
+        break;
+    }
+    robot_feedback.store(robot_info_union.data);
   }
 
+  // この変数は非同期で読み書きするのでatomicにする
+  std::atomic<RobotFeedback> robot_feedback;
+
   boost::asio::io_context io_context;
+
   udp::socket socket;
+
   udp::endpoint sender_endpoint;
+
   enum { max_length = 1024 };
-  char data_[max_length];
+
+  char buffer[max_length];
+
   udp::endpoint endpoint;
+
   const RobotInterfaceConfig config;
 };
 
