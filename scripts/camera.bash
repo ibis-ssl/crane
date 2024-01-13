a3db316aaee43add7b8b66cefd5167d77c2156ec
Kotaro Yoshimoto <pythagora.yoshimoto@gmail.com>
2024-01-13 11:05:53 +0900

    カメラ設定コマンドに破壊的変更はいってた（怒）

--- scripts/camera.bash
+++ scripts/camera.bash
@@ -1,10 +1,15 @@
-device_id = 0
-v4l2-ctl -d $device_id -c exposure_dynamic_framerate=0
-v4l2-ctl -d $device_id -c auto_exposure=1
-v4l2-ctl -d $device_id -c focus_automatic_continuous=0
+device_id=0
+#v4l2-ctl -d $device_id -c exposure_dynamic_framerate=0
+# manual: 1
+v4l2-ctl -d $device_id -c exposure_auto=1
+#v4l2-ctl -d $device_id -c exposure_time_absolute=120
+v4l2-ctl -d $device_id -c exposure_absolute=170
+
+#v4l2-ctl -d $device_id -c focus_automatic_continuous=0
+v4l2-ctl -d $device_id -c focus_auto=0
 v4l2-ctl -d $device_id -c focus_absolute=0
 
 
-v4l2-ctl -d $device_id -c exposure_time_absolute=120
+
 v4l2-ctl -d $device_id -c brightness=120
 v4l2-ctl -d $device_id -c sharpness=400
