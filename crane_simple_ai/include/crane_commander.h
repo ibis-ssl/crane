8d76cd6d4a0631ecafb97bb513b1eee1e594d5c7
Kotaro Yoshimoto <pythagora.yoshimoto@gmail.com>
2024-01-27 17:55:29 +0900

    コミット漏れ

---
+++ crane_simple_ai/include/crane_commander.h
@@ -0,0 +1,103 @@
+#ifndef QT_COMMUNICATION_TESTER_H
+#define QT_COMMUNICATION_TESTER_H
+
+#include <QDataStream>
+#include <QDebug>
+#include <QFile>
+#include <QGraphicsScene>
+#include <QHostAddress>
+#include <QKeyEvent>
+#include <QMainWindow>
+#include <QNetworkDatagram>
+#include <QNetworkInterface>
+#include <QTextStream>
+#include <QThread>
+#include <QTimer>
+#include <QUdpSocket>
+#include <QtGlobal>
+#include <cmath>
+#include <cstdio>
+#include <queue>
+#include <rclcpp/rclcpp.hpp>
+
+QT_BEGIN_NAMESPACE
+namespace Ui
+{
+class CraneCommander;
+}
+QT_END_NAMESPACE
+
+struct Task
+{
+  Task(std::string str)
+  {
+    // ex: "move_to(1.0, 2.0, 3.0)"
+    name = str.substr(0, str.find("("));
+    auto args_str = str.substr(str.find("(") + 1, str.find(")") - str.find("(") - 1);
+    // into args(std::vector<double>)
+    while (args_str.find(",") != std::string::npos) {
+      auto arg_str = args_str.substr(0, args_str.find(","));
+      args.push_back(std::stod(arg_str));
+      args_str = args_str.substr(args_str.find(",") + 1);
+    }
+    // last arg from "<last arg>)"
+    auto arg_str = args_str.substr(0, args_str.find(")"));
+    args.push_back(std::stod(arg_str));
+  }
+  std::string getText() const
+  {
+    // ex1: "move_to(1.0, 2.0, 3.0)"
+    // ex1: "set_kicker_power(1.0)"
+    std::string str = name + "(";
+    for (auto arg : args) {
+      str += std::to_string(arg) + ",";
+    }
+    // remove last ","
+    str = str.substr(0, str.size() - 1);
+    str += ")";
+    return str;
+  }
+  std::string name;
+  std::vector<double> args;
+};
+
+class ROSNode : public rclcpp::Node
+{
+public:
+  ROSNode() : Node("crane_commander") {}
+  ~ROSNode() {}
+};
+
+class CraneCommander : public QMainWindow
+{
+  Q_OBJECT
+
+public:
+  CraneCommander(QWidget * parent = nullptr);
+  ~CraneCommander();
+  void setupROS2();
+
+  void finishROS2() { rclcpp::shutdown(); }
+
+public slots:
+  //    void timer_callback( int time_counter)
+
+private slots:
+  void on_commandAddPushButton_clicked();
+  void on_executionPushButton_clicked();
+
+protected:
+  bool eventFilter(QObject * object, QEvent * event);
+
+private:
+  bool eventKeyPress(QKeyEvent * event);
+  bool eventKeyRelease(QKeyEvent * event);
+
+private:
+  Ui::CraneCommander * ui;
+  QTimer ros_update_timer;
+  std::shared_ptr<ROSNode> ros_node;
+  std::deque<Task> task_queue;
+};
+
+#endif  // QT_COMMUNICATION_TESTER_H
---
+++ crane_simple_ai/src/crane_commander.cpp
@@ -0,0 +1,112 @@
+#include "crane_commander.h"
+
+#include <rclcpp/rclcpp.hpp>
+#include <sstream>
+#include <string>
+
+#include "ui_qt_form.h"
+
+CraneCommander::CraneCommander(QWidget * parent) : QMainWindow(parent), ui(new Ui::CraneCommander)
+{
+  ui->setupUi(this);
+  setupROS2();
+
+  //    QObject::connect(&thread_time, SIGNAL(data_update(int)),this, SLOT(timer_callback(int)) );
+  installEventFilter(this);
+}
+
+CraneCommander::~CraneCommander()
+{
+  finishROS2();
+  delete ui;
+}
+
+bool CraneCommander::eventKeyPress(QKeyEvent * event) { return true; }
+
+bool CraneCommander::eventKeyRelease(QKeyEvent * event) { return true; }
+
+bool CraneCommander::eventFilter(QObject *, QEvent * event)
+{
+  bool bRtn = false;
+
+  //    if(event->type() ==  QEvent::WindowDeactivate){
+  //        ai_cmd.local_target_speed[0]=0.0;
+  //        ai_cmd.local_target_speed[1]=0.0;
+  //    }
+  //    else{
+  //        if (event->type() == QEvent::KeyPress) {
+  //            bRtn = eventKeyPress(static_cast<QKeyEvent *>(event));
+  //        }
+  //        else if (event->type() == QEvent::KeyRelease) {
+  //            bRtn = eventKeyRelease(static_cast<QKeyEvent *>(event));
+  //        }
+  //    }
+
+  return bRtn;
+}
+void CraneCommander::on_commandAddPushButton_clicked()
+{
+  std::stringstream command_ss;
+  command_ss << ui->commandComboBox->currentText().toStdString() << "(";
+
+  if (ui->arg1LineEdit->text() != "") {
+    command_ss << ui->arg1LineEdit->text().toStdString();
+  }
+  if (ui->arg2LineEdit->text() != "") {
+    command_ss << ", " << ui->arg2LineEdit->text().toStdString();
+  }
+  if (ui->arg3LineEdit->text() != "") {
+    command_ss << ", " << ui->arg3LineEdit->text().toStdString();
+  }
+  command_ss << ")\n";
+  // 通常時はTextEditに追加
+  if (task_queue.empty()) {
+    auto command_queue_str = ui->commandQueuePlainTextEdit->toPlainText();
+    command_queue_str += QString::fromStdString(command_ss.str());
+    ui->commandQueuePlainTextEdit->setPlainText(command_queue_str);
+    return;
+  } else {
+    // 実行中はqueueに直接追加
+    task_queue.emplace_back(command_ss.str());
+  }
+}
+
+void CraneCommander::on_executionPushButton_clicked()
+{
+  if (ui->executionPushButton->text() == "実行") {
+    auto command_queue = ui->commandQueuePlainTextEdit->toPlainText().split("\n");
+    for (const auto & command_str : command_queue) {
+      if (command_str == "") {
+        continue;
+      }
+      task_queue.emplace_back(command_str.toStdString());
+    }
+    ui->commandQueuePlainTextEdit->setEnabled(false);
+  } else if (ui->executionPushButton->text() == "停止") {
+    task_queue.clear();
+    ui->commandQueuePlainTextEdit->setEnabled(true);
+  }
+}
+
+void CraneCommander::setupROS2()
+{
+  ros_node = std::make_shared<ROSNode>();
+  ros_update_timer.setInterval(10);  // 100 Hz
+  QObject::connect(&ros_update_timer, &QTimer::timeout, [&]() {
+    if (task_queue.empty()) {
+      ui->commandQueuePlainTextEdit->setReadOnly(false);
+      ui->executionPushButton->setText("実行");
+    } else {
+      // print all task in queue
+      ui->commandQueuePlainTextEdit->setReadOnly(true);
+      ui->executionPushButton->setText("停止");
+      std::stringstream ss;
+      for (const auto & task : task_queue) {
+        ss << task.getText() << "\n";
+      }
+      ui->commandQueuePlainTextEdit->setPlainText(QString::fromStdString(ss.str()));
+    }
+    rclcpp::spin_some(ros_node);
+  });
+  ros_update_timer.start();
+}
