f12c1d790b03a4f838fb7d86e1832d78642f19c5
Kotaro Yoshimoto <pythagora.yoshimoto@gmail.com>
2024-01-31 08:01:24 +0900

    MoveWithBallスキルのパラメータ対応

--- crane_robot_skills/include/crane_robot_skills/move_with_ball.hpp
+++ crane_robot_skills/include/crane_robot_skills/move_with_ball.hpp
@@ -22,26 +22,36 @@ public:
   explicit MoveWithBall(Pose2D pose, uint8_t id, std::shared_ptr<WorldModelWrapper> & world_model)
   : SkillBase<>("move_with_ball", id, world_model, DefaultStates::DEFAULT), target_pose(pose)
   {
+    setParameter("reach_threshold", 0.1);
+    setParameter("reach_angle_threshold", 0.1);
+    setParameter("ball_lost_timeout", 2.0);
+    setParameter("dribble_power", 0.1);
+    // これ以上の角度をずれて進むと停止する
+    setParameter("moving_direction_tolerance", 0.3);
+    // この時間以上ボールが離れたら停止する
+    setParameter("max_contact_losttime", 0.3);
+    // ドリブル時の目標位置の設置距離
+    setParameter("dribble_target_horizon", 0.2);
     addStateFunction(
       DefaultStates::DEFAULT,
       [this](
         const std::shared_ptr<WorldModelWrapper> & world_model,
         const std::shared_ptr<RobotInfo> & robot,
         crane::RobotCommandWrapper & command) -> SkillBase::Status {
-        if (not robot->ball_contact.findPastContact(2.0)) {
+        if (not robot->ball_contact.findPastContact(getParameter<double>("ball_lost_timeout"))) {
           // ボールが離れたら失敗
           return SkillBase::Status::FAILURE;
         } else if (
-          (robot->pose.pos - target_pose.pos).norm() < 0.1 &&
-          std::abs(getAngleDiff(robot->pose.theta, target_pose.theta)) < 0.1) {
+          (robot->pose.pos - target_pose.pos).norm() < getParameter<double>("reach_threshold") &&
+          std::abs(getAngleDiff(robot->pose.theta, target_pose.theta)) < getParameter<double>("reach_angle_threshold")) {
           command.setTargetPosition(target_pose.pos, target_pose.theta);
-          command.dribble(0.2);
+          command.dribble(0.0);
           // ターゲットに到着したら成功
           return SkillBase::Status::SUCCESS;
         } else {
           command.setTargetPosition(getTargetPoint());
           command.setTargetTheta(getTargetAngle());
-          command.dribble(0.1);
+          command.dribble(getParameter<double>("dribble_power"));
           return SkillBase::Status::RUNNING;
         }
       });
@@ -50,9 +60,9 @@ public:
   Point getTargetPoint()
   {
     // 正しい方向でドリブルできている場合だけ前進
-    if (getAngleDiff(robot->pose.theta, getTargetAngle()) < 0.3) {
-      if (robot->ball_contact.findPastContact(0.5)) {
-        return robot->pose.pos + (target_pose.pos - robot->pose.pos).normalized() * 0.2;
+    if (getAngleDiff(robot->pose.theta, getTargetAngle()) < getParameter<double>("moving_direction_tolerance")) {
+      if (robot->ball_contact.findPastContact(getParameter<double>("max_contact_losttime"))) {
+        return robot->pose.pos + (target_pose.pos - robot->pose.pos).normalized() * getParameter<double>("dribble_target_horizon");
       }
     }
     return robot->pose.pos;
