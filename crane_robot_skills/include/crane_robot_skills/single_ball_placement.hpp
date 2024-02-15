d1408f0b4bb6534d9579e7d8dfb5e73319fa1060
Kotaro Yoshimoto <pythagora.yoshimoto@gmail.com>
2024-02-15 09:42:16 +0900

    GoOverBallの導入

--- crane_robot_skills/include/crane_robot_skills/single_ball_placement.hpp
+++ crane_robot_skills/include/crane_robot_skills/single_ball_placement.hpp
@@ -9,6 +9,7 @@
 
 #include <crane_geometry/eigen_adapter.hpp>
 #include "get_ball_contact.hpp"
+#include "go_over_ball.hpp"
 #include "move_with_ball.hpp"
 #include "sleep.hpp"
 #include <crane_robot_skills/skill_base.hpp>
@@ -17,7 +18,7 @@ namespace crane::skills
 {
 enum class SingleBallPlacementStates
 {
-  DEFAULT,
+  GO_OVER_BALL,
   MOVE_TO_TARGET,
   PLACE_BALL,
   SLEEP
@@ -26,24 +27,31 @@ enum class SingleBallPlacementStates
 class SingleBallPlacement : public SkillBase<SingleBallPlacementStates>
 {
 private:
+  std::shared_ptr<GoOverBall> go_over_ball;
+
   std::shared_ptr<GetBallContact> get_ball_contact;
+
   std::shared_ptr<MoveWithBall> move_with_ball;
-  std::shared_ptr<Sleep> sleep;
+
+  std::shared_ptr<Sleep> sleep = nullptr;
 public:
 
   explicit SingleBallPlacement(uint8_t id, std::shared_ptr<WorldModelWrapper> & world_model)
-  : SkillBase<SingleBallPlacementStates>("SingleBallPlacement", id, world_model, SingleBallPlacementStates::DEFAULT)
+  : SkillBase<SingleBallPlacementStates>("SingleBallPlacement", id, world_model, SingleBallPlacementStates::GO_OVER_BALL)
   {
 //    setParameter("stop_by_position", true);
    get_ball_contact = std::make_shared<GetBallContact>(id, world_model);
       move_with_ball = std::make_shared<MoveWithBall>(id, world_model);
         sleep = std::make_shared<Sleep>(id, world_model);
     addStateFunction(
-          SingleBallPlacementStates::DEFAULT,
+          SingleBallPlacementStates::GO_OVER_BALL,
       [this](
         const std::shared_ptr<WorldModelWrapper> & world_model,
         const std::shared_ptr<RobotInfo> & robot, crane::RobotCommandWrapper & command,
         ConsaiVisualizerWrapper::SharedPtr visualizer) -> Status {
+            if(not go_over_ball){
+              go_over_ball = std::make_shared<GoOverBall>(robot->id, world_model);
+            }
 
         return Status::RUNNING;
       });
