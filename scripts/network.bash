54bd4ba2506e6e2d2385aa837114219f83efc1ff
Kotaro Yoshimoto <pythagora.yoshimoto@gmail.com>
2024-02-20 08:57:13 +0900

    フォーマット

--- scripts/network.bash
+++ scripts/network.bash
@@ -3,4 +3,3 @@ robot_wifi_device="enp4s0"
 sudo iptables -A FORWARD -o $robot_wifi_device -d 224.5.23.2 -j DROP
 # Refereeパケットをロボットに送信しない
 sudo iptables -A FORWARD -o $robot_wifi_device -d 224.5.23.1 -j DROP
-
