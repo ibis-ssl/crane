984835b8f9763151b531ee41bf23be8cfb6e4d2f
Kotaro Yoshimoto <pythagora.yoshimoto@gmail.com>
2024-02-22 07:48:55 +0900

    ビルドエラー修正

--- crane_robot_skills/include/crane_robot_skills/penalty_kick.hpp
+++ crane_robot_skills/include/crane_robot_skills/penalty_kick.hpp
@@ -27,22 +27,18 @@ private:
   std::optional<Point> start_ball_point = std::nullopt;
 
 public:
-  explicit PenaltyKick(uint8_t id, const std::shared_ptr<WorldModelWrapper> & world_model)
-  : SkillBase<PenaltyKickState>("PenaltyKick", id, world_model, PenaltyKickState::PREPARE)
+  explicit PenaltyKick(uint8_t id, const std::shared_ptr<WorldModelWrapper> & wm)
+  : SkillBase<PenaltyKickState>("PenaltyKick", id, wm, PenaltyKickState::PREPARE)
   {
     setParameter("start_from_kick", false);
     setParameter("prepare_margin", 0.6);
     addStateFunction(
-      PenaltyKickState::PREPARE,
-      [this](
-        const std::shared_ptr<WorldModelWrapper> & world_model,
-        const std::shared_ptr<RobotInfo> & robot, crane::RobotCommandWrapper & command,
-        ConsaiVisualizerWrapper::SharedPtr visualizer) -> Status {
+      PenaltyKickState::PREPARE, [this](ConsaiVisualizerWrapper::SharedPtr visualizer) -> Status {
         Point target = world_model->ball.pos;
         auto margin = getParameter<double>("prepare_margin");
         target.x() += world_model->getOurGoalCenter().x() > 0 ? -margin : margin;
-        command.setTargetPosition(target);
-        command.lookAtBall();
+        command->setTargetPosition(target);
+        command->lookAtBall();
         return Status::RUNNING;
       });
 
@@ -55,11 +51,7 @@ public:
       }
     });
     addStateFunction(
-      PenaltyKickState::KICK,
-      [this](
-        const std::shared_ptr<WorldModelWrapper> & world_model,
-        const std::shared_ptr<RobotInfo> & robot, crane::RobotCommandWrapper & command,
-        ConsaiVisualizerWrapper::SharedPtr visualizer) -> Status {
+      PenaltyKickState::KICK, [this](ConsaiVisualizerWrapper::SharedPtr visualizer) -> Status {
         if (not start_ball_point) {
           start_ball_point = world_model->ball.pos;
         }
@@ -76,16 +68,16 @@ public:
         double target_theta = getAngle(best_target - world_model->ball.pos);
         // ボールと敵ゴールの延長線上にいない && 角度があってないときは，中間ポイントを経由
         if (dot < 0.95 || std::abs(getAngleDiff(target_theta, robot->pose.theta)) > 0.05) {
-          command.setTargetPosition(intermediate_point);
-          command.enableCollisionAvoidance();
+          command->setTargetPosition(intermediate_point);
+          command->enableCollisionAvoidance();
         } else {
-          command.setTargetPosition(world_model->ball.pos);
-          command.kickStraight(0.7).disableCollisionAvoidance();
-          command.enableCollisionAvoidance();
-          command.disableBallAvoidance();
+          command->setTargetPosition(world_model->ball.pos);
+          command->kickStraight(0.7).disableCollisionAvoidance();
+          command->enableCollisionAvoidance();
+          command->disableBallAvoidance();
         }
 
-        command.setTargetTheta(getAngle(best_target - world_model->ball.pos));
+        command->setTargetTheta(getAngle(best_target - world_model->ball.pos));
       });
 
     addTransition(PenaltyKickState::KICK, PenaltyKickState::DONE, [this]() {
@@ -93,12 +85,8 @@ public:
     });
 
     addStateFunction(
-      PenaltyKickState::DONE,
-      [this](
-        const std::shared_ptr<WorldModelWrapper> & world_model,
-        const std::shared_ptr<RobotInfo> & robot, crane::RobotCommandWrapper & command,
-        ConsaiVisualizerWrapper::SharedPtr visualizer) -> Status {
-        command.stopHere();
+      PenaltyKickState::DONE, [this](ConsaiVisualizerWrapper::SharedPtr visualizer) -> Status {
+        command->stopHere();
         return Status::RUNNING;
       });
   }
