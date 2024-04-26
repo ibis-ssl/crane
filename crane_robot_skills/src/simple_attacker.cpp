4a29a33bb21d279bce642068b17d116b4b4e3c32
Kotaro Yoshimoto <pythagora.yoshimoto@gmail.com>
2024-04-27 00:59:50 +0900

    回り込み判定を微調整

--- crane_robot_skills/src/simple_attacker.cpp
+++ crane_robot_skills/src/simple_attacker.cpp
@@ -51,24 +51,25 @@ SimpleAttacker::SimpleAttacker(uint8_t id, const std::shared_ptr<WorldModelWrapp
         }
       }
 
-      //      Point ball_pos = world_model->ball.pos + world_model->ball.vel * 0.5;
+      Point ball_pos = world_model->ball.pos + world_model->ball.vel * 0.0;
       // 経由ポイント
       Point intermediate_point =
-        world_model->ball.pos + (world_model->ball.pos - best_target).normalized() * 0.2;
+        ball_pos + (ball_pos - best_target).normalized() * 0.3;
 
-      double dot = (robot->pose.pos - world_model->ball.pos)
+      double dot = (robot->pose.pos - ball_pos)
                      .normalized()
-                     .dot((world_model->ball.pos - best_target).normalized());
-      double target_theta = getAngle(best_target - world_model->ball.pos);
+                     .dot((ball_pos - best_target).normalized());
+      double target_theta = getAngle(best_target - ball_pos);
       // ボールと敵ゴールの延長線上にいない && 角度があってないときは，中間ポイントを経由
-      if (dot < 0.9 || std::abs(getAngleDiff(target_theta, robot->pose.theta)) > 0.2) {
+      if ((dot < 0.9 && (robot->pose.pos - ball_pos).norm() > 0.1) || std::abs(getAngleDiff(target_theta, robot->pose.theta)) > 0.2) {
         command->setTargetPosition(intermediate_point);
         command->enableCollisionAvoidance();
+        command->enableBallAvoidance();
         // ワンタッチシュート時にキックできるようにキッカーをONにしておく
-        command->kickStraight(0.3);
+        command->kickStraight(0.5);
       } else {
-        command->setTargetPosition(world_model->ball.pos);
-        command->kickStraight(0.3).disableCollisionAvoidance();
+        command->setTargetPosition(ball_pos + (best_target - ball_pos).normalized() * 0.5);
+        command->kickStraight(0.5).disableCollisionAvoidance();
         command->enableCollisionAvoidance();
         command->disableBallAvoidance();
       }
