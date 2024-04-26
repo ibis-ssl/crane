96b61301d815a7d85f98710c1104915d7004fbc8
Kotaro Yoshimoto <pythagora.yoshimoto@gmail.com>
2024-04-27 03:06:59 +0900

    コメントを日本語に

--- crane_local_planner/src/gridmap_planner.cpp
+++ crane_local_planner/src/gridmap_planner.cpp
@@ -383,7 +383,7 @@ crane_msgs::msg::RobotCommands GridMapPlanner::calculateRobotCommand(
 
       if (not command.local_planner_config.disable_collision_avoidance) {
         map[map_name] += map.get("friend_robot");
-        // delete current robot position
+        // 使うロボットの位置が障害物にならないようにする
         for (grid_map::CircleIterator iterator(map, robot->pose.pos, 0.31); !iterator.isPastEnd();
              ++iterator) {
           map.at(map_name, *iterator) = 0.;
