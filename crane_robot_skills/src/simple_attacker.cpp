e80aa5a96514464dbce5fe6cdae0319753e74cba
Kotaro Yoshimoto <pythagora.yoshimoto@gmail.com>
2024-05-03 18:05:54 +0900

    WIP

--- crane_robot_skills/src/simple_attacker.cpp
+++ crane_robot_skills/src/simple_attacker.cpp
@@ -55,23 +55,7 @@ SimpleAttacker::SimpleAttacker(uint8_t id, const std::shared_ptr<WorldModelWrapp
         }
       }();
 
-      Point ball_pos = world_model->ball.pos + world_model->ball.vel * 0.0;
-      // 経由ポイント
-      Point intermediate_point = ball_pos + (ball_pos - best_target).normalized() * 0.3;
-      intermediate_point += (intermediate_point - robot->pose.pos) * 0.1;
-
-      double dot =
-        (robot->pose.pos - ball_pos).normalized().dot((ball_pos - best_target).normalized());
-      double target_theta = getAngle(best_target - ball_pos);
-      // ボールと敵ゴールの延長線上にいない && 角度があってないときは，中間ポイントを経由
-      if (
-        (dot < 0.95 && (robot->pose.pos - ball_pos).norm() > 0.1) ||
-        std::abs(getAngleDiff(target_theta, robot->pose.theta)) > 0.2) {
-        command->setTargetPosition(intermediate_point);
-        command->enableCollisionAvoidance();
-        command->enableBallAvoidance();
-        // ワンタッチシュート時にキックできるようにキッカーをONにしておく
-        command->kickStraight(0.8);
+      if (world_model->ball.vel.norm() > 0.5) {
         // 後ろからきたボールは一旦避ける
         Segment ball_line{ball_pos, ball_pos + world_model->ball.vel * 3.0};
         ClosestPoint result;
@@ -87,24 +71,62 @@ SimpleAttacker::SimpleAttacker(uint8_t id, const std::shared_ptr<WorldModelWrapp
           command->setTargetPosition(
             result.closest_point + (robot->pose.pos - result.closest_point).normalized() * 0.5);
           command->enableBallAvoidance();
+        } else {
+          // ボールにスピードがあるときは立ちふさがるように
+          // 経由ポイント
+          Point intermediate_point = ball_pos + world_model->ball.vel * 0.3;
+          intermediate_point += (intermediate_point - robot->pose.pos) * 0.1;
+          double dot =
+            (robot->pose.pos - ball_pos).normalized().dot((ball_pos - best_target).normalized());
+          double target_theta = getAngle(best_target - ball_pos);
+          // ボールと敵ゴールの延長線上にいない && 角度があってないときは，中間ポイントを経由
+          if (
+            (dot < 0.95 && (robot->pose.pos - ball_pos).norm() > 0.1) ||
+            std::abs(getAngleDiff(target_theta, robot->pose.theta)) > 0.2) {
+            command->setTargetPosition(intermediate_point);
+            command->enableCollisionAvoidance();
+            command->enableBallAvoidance();
+            // ワンタッチシュート時にキックできるようにキッカーをONにしておく
+            command->kickStraight(0.8);
+          }
         }
       } else {
-        command->setTargetPosition(ball_pos + (best_target - ball_pos).normalized() * 0.5);
-        command->kickStraight(0.8).disableCollisionAvoidance();
-        command->enableCollisionAvoidance();
-        command->disableBallAvoidance();
-      }
-      command->setTerminalVelocity(world_model->ball.vel.norm() * 3.0);
-      command->liftUpDribbler();
-      command->setTargetTheta(getAngle(best_target - world_model->ball.pos));
+        Point ball_pos = world_model->ball.pos + world_model->ball.vel * 0.0;
+        // 経由ポイント
+        Point intermediate_point = ball_pos + (ball_pos - best_target).normalized() * 0.3;
+        intermediate_point += (intermediate_point - robot->pose.pos) * 0.1;
 
-      bool is_in_defense = world_model->isDefenseArea(world_model->ball.pos);
-      bool is_in_field = world_model->isFieldInside(world_model->ball.pos);
+        double dot =
+          (robot->pose.pos - ball_pos).normalized().dot((ball_pos - best_target).normalized());
+        double target_theta = getAngle(best_target - ball_pos);
+        // ボールと敵ゴールの延長線上にいない && 角度があってないときは，中間ポイントを経由
+        if (
+          (dot < 0.95 && (robot->pose.pos - ball_pos).norm() > 0.1) ||
+          std::abs(getAngleDiff(target_theta, robot->pose.theta)) > 0.2) {
+          command->setTargetPosition(intermediate_point);
+          command->enableCollisionAvoidance();
+          command->enableBallAvoidance();
+          // ワンタッチシュート時にキックできるようにキッカーをONにしておく
+          command->kickStraight(0.8);
 
-      if ((not is_in_field) or is_in_defense) {
-        command->stopHere();
+        } else {
+          command->setTargetPosition(ball_pos + (best_target - ball_pos).normalized() * 0.5);
+          command->kickStraight(0.8).disableCollisionAvoidance();
+          command->enableCollisionAvoidance();
+          command->disableBallAvoidance();
+        }
+        command->setTerminalVelocity(world_model->ball.vel.norm() * 3.0);
+        command->liftUpDribbler();
+        command->setTargetTheta(getAngle(best_target - world_model->ball.pos));
+
+        bool is_in_defense = world_model->isDefenseArea(world_model->ball.pos);
+        bool is_in_field = world_model->isFieldInside(world_model->ball.pos);
+
+        if ((not is_in_field) or is_in_defense) {
+          command->stopHere();
+        }
+        return Status::RUNNING;
       }
-      return Status::RUNNING;
     });
 }
 }  // namespace crane::skills
