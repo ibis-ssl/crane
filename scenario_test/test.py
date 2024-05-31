2f680b0f708277679a01ba0205830a36e3d49c90
Kotaro Yoshimoto <pythagora.yoshimoto@gmail.com>
2024-06-01 02:35:53 +0900

    STOPするテスト

--- scenario_test/test.py
+++ scenario_test/test.py
@@ -1,3 +1,4 @@
+import math
 import time
 from rcst.communication import Communication
 from rcst import calc
@@ -5,34 +6,56 @@ from rcst.ball import Ball
 from rcst.robot import RobotDict
 
 
-def test(rcst_comm: Communication):
+def test_robot_speed(rcst_comm: Communication):
     rcst_comm.send_empty_world()
-    rcst_comm.send_ball(0, 0)
+    ball_x = 1.0
+    rcst_comm.send_ball(ball_x, 0)
     for i in range(11):
-        rcst_comm.send_blue_robot(i, -1.0, 3.0 - i * 0.5, 0)
-    rcst_comm.change_referee_command("STOP", 3.0)
+        rcst_comm.send_blue_robot(i, -1.0, 3.0 - i * 0.5, math.radians(0))
+
+    rcst_comm.change_referee_command('STOP', 3.0)
+
     rcst_comm.observer.reset()
     success = True
-    rcst_comm.send_ball(0, 0, 5.0, 0.0)  # Move the ball
+    rcst_comm.send_ball(ball_x, 0, 5.0, 0.0)  # Move the ball
     for _ in range(5):
-        if rcst_comm.observer.robot_speed().some_blue_robots_over(0.0):
+        if rcst_comm.observer.robot_speed().some_blue_robots_over(1.5):
             success = False
             break
         time.sleep(1)
     assert success is True
 
 
-def force_start_test(rcst_comm: Communication):
+def test_avoid_ball(rcst_comm: Communication):
     rcst_comm.send_empty_world()
-    rcst_comm.send_ball(0, 0)
     for i in range(11):
-        rcst_comm.send_blue_robot(i, -1.0, 3.0 - i * 0.5, 0)
-    rcst_comm.change_referee_command("FORCE_START", 3.0)
-    rcst_comm.observer.reset()
-    success = True
-    rcst_comm.send_ball(0, 0, 5.0, 0.0)  # Move the ball
-    for _ in range(5):
-        if rcst_comm.observer.robot_speed().some_blue_robots_over(0.0):
-            success = False
-        time.sleep(1)
-    assert success is True
+        rcst_comm.send_blue_robot(i, -1.0, 3.0 - i * 0.5, math.radians(0))
+
+    def blue_robot_did_not_avoid_ball(
+            ball: Ball, blue_robots: RobotDict, yellow_robots: RobotDict) -> bool:
+        for robot in blue_robots.values():
+            if calc.distance_robot_and_ball(robot, ball) < 0.4:
+                return True
+        return False
+
+    rcst_comm.observer.customized().register_sticky_true_callback(
+        "blue_robot_did_not_avoid_ball", blue_robot_did_not_avoid_ball)
+
+    rcst_comm.change_referee_command('STOP', 1.0)
+
+    def check(x: float, y: float, vx: float = 0.0, vy: float = 0.0):
+        rcst_comm.send_ball(x, y, vx, vy)
+        time.sleep(2)
+        rcst_comm.observer.reset()
+
+        success = True
+        for _ in range(5):
+            if rcst_comm.observer.customized().get_result("blue_robot_did_not_avoid_ball"):
+                success = False
+                break
+            time.sleep(1)
+        assert success is True
+
+    check(0, 0)
+    check(-3.5, 0)
+    check(-3.5, 0, vx=2.0)
