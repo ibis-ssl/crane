4dc76d9f099b59960cd66ebdf10cfc7891c5e4bf
Kotaro Yoshimoto <pythagora.yoshimoto@gmail.com>
2024-06-01 03:25:18 +0900

    テストを分割

--- scenario_test/STOP_AVOID_BALL.py
+++ scenario_test/STOP_AVOID_BALL.py
@@ -6,41 +6,21 @@ from rcst.ball import Ball
 from rcst.robot import RobotDict
 
 
-def test_robot_speed(rcst_comm: Communication):
-    rcst_comm.send_empty_world()
-    ball_x = 1.0
-    rcst_comm.send_ball(ball_x, 0)
-    for i in range(11):
-        rcst_comm.send_blue_robot(i, -1.0, 3.0 - i * 0.5, math.radians(0))
-
-    rcst_comm.change_referee_command("STOP", 3.0)
-
-    rcst_comm.observer.reset()
-    success = True
-    rcst_comm.send_ball(ball_x, 0, 5.0, 0.0)  # Move the ball
-    for _ in range(5):
-        if rcst_comm.observer.robot_speed().some_blue_robots_over(1.5):
-            success = False
-            break
-        time.sleep(1)
-    assert False is True
-
-
 def test_avoid_ball(rcst_comm: Communication):
     rcst_comm.send_empty_world()
     for i in range(11):
-        rcst_comm.send_blue_robot(i, -1.0, 3.0 - i * 0.5, math.radians(0))
+        rcst_comm.send_yellow_robot(i, -1.0, 3.0 - i * 0.5, math.radians(0))
 
-    def blue_robot_did_not_avoid_ball(
+    def yellow_robot_did_not_avoid_ball(
         ball: Ball, blue_robots: RobotDict, yellow_robots: RobotDict
     ) -> bool:
-        for robot in blue_robots.values():
+        for robot in yellow_robots.values():
             if calc.distance_robot_and_ball(robot, ball) < 0.4:
                 return True
         return False
 
     rcst_comm.observer.customized().register_sticky_true_callback(
-        "blue_robot_did_not_avoid_ball", blue_robot_did_not_avoid_ball
+        "yellow_robot_did_not_avoid_ball", yellow_robot_did_not_avoid_ball
     )
 
     rcst_comm.change_referee_command("STOP", 1.0)
@@ -52,7 +32,7 @@ def test_avoid_ball(rcst_comm: Communication):
 
         success = True
         for _ in range(5):
-            if rcst_comm.observer.customized().get_result("blue_robot_did_not_avoid_ball"):
+            if rcst_comm.observer.customized().get_result("yellow_robot_did_not_avoid_ball"):
                 success = False
                 break
             time.sleep(1)
