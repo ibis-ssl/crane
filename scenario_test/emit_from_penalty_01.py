7e7d7abc72dde2b0c646d2460f8c4e956e1439bd
Kotaro Yoshimoto <pythagora.yoshimoto@gmail.com>
2024-06-14 21:16:56 +0900

    パラメータミスってた

--- scenario_test/emit_from_penalty_01.py
+++ scenario_test/emit_from_penalty_01.py
@@ -7,16 +7,16 @@ from rcst.robot import RobotDict
 
 
 def is_in_penalty_area(x: float, y: float) -> bool:
-    return math.fabs(x) >= 8.0 and math.fabs(y) <= 1.0
+    return math.fabs(x) >= 6.0 and math.fabs(y) <= 1.8
 
 
 def test_emit_from_penalty_01(rcst_comm: Communication):
     rcst_comm.send_empty_world()
-    rcst_comm.send_ball(8.5, 0.5)
-    rcst_comm.send_yellow_robot(0, 9.0, 0, 0)
+    rcst_comm.send_ball(5.0, 1.0)
+    rcst_comm.send_yellow_robot(0, 6.0, 0, 0)
     rcst_comm.change_referee_command("FORCE_START", 3.0)
 
     rcst_comm.observer.reset()
-    time.sleep(10)
-    success = not is_in_penalty_area(rcst_comm.observer.ball().x, rcst_comm.observer.ball().y)
+    time.sleep(5)
+    success = not is_in_penalty_area(rcst_comm.observer.get_world().get_ball().x, rcst_comm.observer.get_world().get_ball().y)
     assert False is True
