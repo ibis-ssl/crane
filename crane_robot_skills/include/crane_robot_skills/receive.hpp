6be715a94ce51fa92da0df6c64283b9d43a95abb
Kotaro Yoshimoto <pythagora.yoshimoto@gmail.com>
2024-07-19 01:15:18 +0900

    Receiveスキル仮実装

--- crane_robot_skills/include/crane_robot_skills/receive.hpp
+++ crane_robot_skills/include/crane_robot_skills/receive.hpp
@@ -19,13 +19,64 @@ public:
   explicit Receive(uint8_t id, const std::shared_ptr<WorldModelWrapper> & wm)
   : SkillBase<>("Receive", id, wm, DefaultStates::DEFAULT)
   {
+    setParameter("receive_policy", "closest");
+    setParameter("next_action", "trap");
+    setParameter("dribble_power", 0.5);
+    setParameter("redirect_robot_theta", 0.0);
+    setParameter("redirect_chip", false);
+    setParameter("redirect_kick_power", 0.5);
     addStateFunction(
       DefaultStates::DEFAULT,
       [this](const ConsaiVisualizerWrapper::SharedPtr & visualizer) -> Status {
         // ボールラインに乗る（ポリシー：最近傍・MaxSlack・MinSlack）
         // 受け取った瞬間にやること（ドリブルしてトラップ・リダイレクト（角度・キック力指定））
+        auto next_action = getParameter<std::string>("next_action");
+        auto dribble_power = getParameter<double>("dribble_power");
+        auto receive_point = getReceivePoint();
+        if (next_action == "trap") {
+          command->setTargetPosition(receive_point)
+            .lookAtBallFrom(receive_point)
+            .disableBallAvoidance()
+            .dribble(dribble_power);
+        } else if (next_action == "redirect") {
+          auto redirect_robot_theta = getParameter<double>("redirect_robot_theta");
+          auto redirect_kick_power = getParameter<double>("redirect_kick_power");
+          auto redirect_chip = getParameter<bool>("redirect_chip");
+          auto target = receive_point + Point(cos(redirect_robot_theta), sin(redirect_robot_theta));
+          command->setTargetTheta(redirect_robot_theta)
+            .setDribblerTargetPosition(receive_point)
+            .dribble(dribble_power)
+            .disableBallAvoidance();
+          if (redirect_chip) {
+            command->kickWithChip(redirect_kick_power);
+          } else {
+            command->kickStraight(redirect_kick_power);
+          }
+        } else {
+          throw std::runtime_error("unexpected string for next_action is detected");
+        }
+
         return Status::RUNNING;
       });
+  }
+
+  auto getReceivePoint() -> Point
+  {
+    auto policy = getParameter<std::string>("receive_policy");
+    if (policy == "closest") {
+      Segment ball_line(
+        world_model->ball.pos,
+        (world_model->ball.pos +
+         world_model->ball.vel.normalized() * (world_model->ball.pos - robot->pose.pos).norm()));
+      auto result = getClosestPointAndDistance(robot->pose.pos, ball_line);
+      return result.closest_point;
+    } else if (policy == "max_slack") {
+      throw std::runtime_error("max_slack for receive_policy is not implemented");
+    } else if (policy == "min_slack") {
+      throw std::runtime_error("min_slack for receive_policy is not implemented");
+    } else {
+      throw std::runtime_error("unexpected string for receive_policy is detected");
+    }
   }
 
   void print(std::ostream & os) const override { os << "[Receive]"; }
