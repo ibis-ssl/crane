db3e5844adf33a8c94727ebcc7b51aaefa19b7d9
Kotaro Yoshimoto <pythagora.yoshimoto@gmail.com>
2024-07-21 19:06:07 +0900

    ロボットコマンドのバリデーション

--- crane_local_planner/src/crane_local_planner.cpp
+++ crane_local_planner/src/crane_local_planner.cpp
@@ -17,15 +17,65 @@ void LocalPlannerComponent::callbackRobotCommands(const crane_msgs::msg::RobotCo
   }
   ScopedTimer process_timer(process_time_pub);
 
-  crane_msgs::msg::RobotCommands commands = msg;
-  for (auto & command : commands.robot_commands) {
-    auto robot = world_model->getOurRobot(command.robot_id);
-    command.current_pose.x = robot->pose.pos.x();
-    command.current_pose.y = robot->pose.pos.y();
-    command.current_pose.theta = robot->pose.theta;
-    command.current_velocity.x = robot->vel.linear.x();
-    command.current_velocity.y = robot->vel.linear.y();
-    command.current_velocity.theta = robot->vel.omega;
+  crane_msgs::msg::RobotCommands commands;
+  for (const auto & raw_command : msg.robot_commands) {
+    bool is_valid = true;
+    switch (raw_command.control_mode) {
+      case crane_msgs::msg::RobotCommand::LOCAL_CAMERA_MODE:
+        if (raw_command.local_camera_mode.empty()) {
+          is_valid = false;
+          std::stringstream what;
+          what << "The robot " << static_cast<int>(raw_command.robot_id)
+               << " is specified as LOCAL_CAMERA_MODE, but no local_camera_mode is set.";
+          RCLCPP_ERROR(get_logger(), what.str().c_str());
+        }
+        break;
+      case crane_msgs::msg::RobotCommand::POSITION_TARGET_MODE:
+        if (raw_command.position_target_mode.empty()) {
+          is_valid = false;
+          std::stringstream what;
+          what << "The robot " << static_cast<int>(raw_command.robot_id)
+               << " is specified as POSITION_TARGET_MODE, but no position_target_mode is set.";
+          RCLCPP_ERROR(get_logger(), what.str().c_str());
+        }
+        break;
+      case crane_msgs::msg::RobotCommand::SIMPLE_VELOCITY_TARGET_MODE:
+        if (not raw_command.simple_velocity_target_mode.empty()) {
+          is_valid = false;
+          std::stringstream what;
+          what << "The robot " << static_cast<int>(raw_command.robot_id)
+               << " is specified as SIMPLE_VELOCITY_TARGET_MODE, but simple_velocity_target_mode "
+                  "is set.";
+          RCLCPP_ERROR(get_logger(), what.str().c_str());
+        }
+        break;
+      case crane_msgs::msg::RobotCommand::VELOCITY_TARGET_MODE:
+        if (raw_command.velocity_target_mode.empty()) {
+          is_valid = false;
+          std::stringstream what;
+          what << "The robot " << static_cast<int>(raw_command.robot_id)
+               << " is specified as VELOCITY_TARGET_MODE, but no velocity_target_mode is set.";
+          RCLCPP_ERROR(get_logger(), what.str().c_str());
+        }
+      default:
+        is_valid = false;
+        std::stringstream what;
+        what << "The robot " << static_cast<int>(raw_command.robot_id)
+             << " is specified as an unknown control mode.";
+        RCLCPP_ERROR(get_logger(), what.str().c_str());
+        break;
+    }
+    if (is_valid) {
+      crane_msgs::msg::RobotCommand command = raw_command;
+      auto robot = world_model->getOurRobot(command.robot_id);
+      command.current_pose.x = robot->pose.pos.x();
+      command.current_pose.y = robot->pose.pos.y();
+      command.current_pose.theta = robot->pose.theta;
+      command.current_velocity.x = robot->vel.linear.x();
+      command.current_velocity.y = robot->vel.linear.y();
+      command.current_velocity.theta = robot->vel.omega;
+      commands.robot_commands.push_back(command);
+    }
   }
 
   auto pub_msg = calculate_control_target(commands);
