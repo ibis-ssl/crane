7d31b5656ff73f29dd79d44c31417709136a8729
Kotaro Yoshimoto <pythagora.yoshimoto@gmail.com>
2024-07-22 02:24:27 +0900

    ビルドエラー修正

--- crane_robot_skills/include/crane_robot_skills/goal_kick.hpp
+++ crane_robot_skills/include/crane_robot_skills/goal_kick.hpp
@@ -13,52 +13,50 @@
 
 namespace crane::skills
 {
-class GoalKick : public SkillBase<>
+class GoalKick : public SkillBase
 {
 public:
   explicit GoalKick(uint8_t id, const std::shared_ptr<WorldModelWrapper> & wm)
-  : SkillBase<>("GoalKick", id, wm, DefaultStates::DEFAULT)
+  : SkillBase("GoalKick", id, wm)
   {
     setParameter("キック角度の最低要求精度[deg]", 1.0);
-    addStateFunction(
-      DefaultStates::DEFAULT,
-      [this](const ConsaiVisualizerWrapper::SharedPtr & visualizer) -> Status {
-        auto [best_angle, goal_angle_width] =
-          world_model->getLargestGoalAngleRangeFromPoint(world_model->ball.pos);
-        // 隙間のなかで更に良い角度を計算する。
-        // キック角度の最低要求精度をオフセットとしてできるだけ端っこを狙う
+  }
 
-        Point target = world_model->ball.pos + getNormVec(best_angle) * 0.5;
+  Status update(const ConsaiVisualizerWrapper::SharedPtr & visualizer) override
+  {
+    auto [best_angle, goal_angle_width] =
+      world_model->getLargestGoalAngleRangeFromPoint(world_model->ball.pos);
+    // 隙間のなかで更に良い角度を計算する。
+    // キック角度の最低要求精度をオフセットとしてできるだけ端っこを狙う
 
-        {  // この部分はいずれ回り込み＆キックのスキルとして一般化したい
-           // (その時は動くボールへの回り込みを含めて)
-          // パラメータ候補：キックパワー・dotしきい値・角度しきい値・経由ポイント距離・突撃速度
-          auto ball_pos = world_model->ball.pos;
-          double dot =
-            (robot->pose.pos - ball_pos).normalized().dot((ball_pos - target).normalized());
-          if (
-            (dot > 0.95 || (robot->pose.pos - ball_pos).norm() > 0.1) &&
-            std::abs(getAngleDiff(getAngle(target - world_model->ball.pos), robot->pose.theta)) >
-              0.1) {
-            // キック
-            command->setTargetPosition(ball_pos + (target - ball_pos).normalized() * 0.5)
-              .kickStraight(0.8)
-              .disableCollisionAvoidance()
-              .enableCollisionAvoidance()
-              .disableBallAvoidance();
-          } else {
-            // 経由ポイントへGO
-            Point intermediate_point = ball_pos + (ball_pos - target).normalized() * 0.3;
-            command->setTargetPosition(intermediate_point)
-              .enableCollisionAvoidance()
-              .enableBallAvoidance();
-          }
-          // 共通コマンド
-          command->liftUpDribbler().setTargetTheta(getAngle(target - world_model->ball.pos));
-        }
+    Point target = world_model->ball.pos + getNormVec(best_angle) * 0.5;
 
-        return Status::RUNNING;
-      });
+    {  // この部分はいずれ回り込み＆キックのスキルとして一般化したい
+       // (その時は動くボールへの回り込みを含めて)
+      // パラメータ候補：キックパワー・dotしきい値・角度しきい値・経由ポイント距離・突撃速度
+      auto ball_pos = world_model->ball.pos;
+      double dot = (robot->pose.pos - ball_pos).normalized().dot((ball_pos - target).normalized());
+      if (
+        (dot > 0.95 || (robot->pose.pos - ball_pos).norm() > 0.1) &&
+        std::abs(getAngleDiff(getAngle(target - world_model->ball.pos), robot->pose.theta)) > 0.1) {
+        // キック
+        command->setTargetPosition(ball_pos + (target - ball_pos).normalized() * 0.5)
+          .kickStraight(0.8)
+          .disableCollisionAvoidance()
+          .enableCollisionAvoidance()
+          .disableBallAvoidance();
+      } else {
+        // 経由ポイントへGO
+        Point intermediate_point = ball_pos + (ball_pos - target).normalized() * 0.3;
+        command->setTargetPosition(intermediate_point)
+          .enableCollisionAvoidance()
+          .enableBallAvoidance();
+      }
+      // 共通コマンド
+      command->liftUpDribbler().setTargetTheta(getAngle(target - world_model->ball.pos));
+    }
+
+    return Status::RUNNING;
   }
 
   void print(std::ostream & os) const override { os << "[GoalKick] "; }
