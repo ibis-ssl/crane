4608f1c0826657da0eb3cda60dc802030305bff1
Kotaro Yoshimoto <pythagora.yoshimoto@gmail.com>
2024-07-23 01:13:33 +0900

    警告を抑制

--- crane_robot_skills/include/crane_robot_skills/skill_base.hpp
+++ crane_robot_skills/include/crane_robot_skills/skill_base.hpp
@@ -91,11 +91,10 @@ inline std::string getTypeString(const ContextType & type)
   std::string type_string;
   std::visit(
     overloaded{
-      [&](const double e) { type_string = "double"; }, [&](const bool e) { type_string = "bool"; },
-      [&](const int e) { type_string = "int"; },
-      [&](const std::string e) { type_string = "string"; },
-      [&](const Point e) { type_string = "Point"; },
-      [&](const std::optional<Point> e) { type_string = "op<Point>"; }},
+      [&](const double) { type_string = "double"; }, [&](const bool) { type_string = "bool"; },
+      [&](const int) { type_string = "int"; }, [&](const std::string) { type_string = "string"; },
+      [&](const Point) { type_string = "Point"; },
+      [&](const std::optional<Point>) { type_string = "op<Point>"; }},
     type);
   return type_string;
 }
