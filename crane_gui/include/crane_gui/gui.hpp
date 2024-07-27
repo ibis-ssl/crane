05cb718a34cc9692800451d9b8237b59dc985285
T.W <36392290+ssaattww@users.noreply.github.com>
2024-07-27 15:14:45 +0900

    imgui対応 (#470)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Kotaro Yoshimoto <pythagora.yoshimoto@gmail.com>

---
+++ crane_gui/include/crane_gui/gui.hpp
@@ -0,0 +1,45 @@
+// Copyright (c) 2024 ibis-ssl
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+#ifndef CRANE_GUI__GUI_HPP_
+#define CRANE_GUI__GUI_HPP_
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#include <rclcpp/rclcpp.hpp>
+
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+#include "visibility_control.h"
+
+namespace crane
+{
+
+class CraneGuiComponent : public rclcpp::Node
+{
+public:
+  // function
+  COMPOSITION_PUBLIC
+  explicit CraneGuiComponent(const rclcpp::NodeOptions & options);
+  int initializeGL();
+  void initializeImGui();
+
+private:
+  // function
+  void loop();
+
+private:
+  // variable
+  rclcpp::TimerBase::SharedPtr timer_;
+  const char * glsl_version;
+  GLFWwindow * window;
+  ImVec4 clear_color;
+};
+
+}  // namespace crane
+#endif  // CRANE_GUI__GUI_HPP_
