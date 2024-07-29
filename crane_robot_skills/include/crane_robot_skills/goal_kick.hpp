9eb808e701abc9e76162d556cf1cf09c1ed41ffc
Kotaro Yoshimoto <pythagora.yoshimoto@gmail.com>
2024-07-30 07:36:25 +0900

    GoalKickスキルの内部でKickスキルを使用

--- crane_robot_skills/include/crane_robot_skills/goal_kick.hpp
+++ crane_robot_skills/include/crane_robot_skills/goal_kick.hpp
@@ -8,6 +8,7 @@
 #define CRANE_ROBOT_SKILLS__GOAL_KICK_HPP_
 
 #include <crane_basics/eigen_adapter.hpp>
+#include <crane_robot_skills/kick.hpp>
 #include <crane_robot_skills/skill_base.hpp>
 #include <memory>
 
@@ -16,9 +17,14 @@ namespace crane::skills
 class GoalKick : public SkillBase<RobotCommandWrapperPosition>
 {
 public:
-  explicit GoalKick(RobotCommandWrapperBase::SharedPtr & base) : SkillBase("GoalKick", base)
+  explicit GoalKick(RobotCommandWrapperBase::SharedPtr & base)
+  : SkillBase("GoalKick", base), kick_skill(base)
   {
     setParameter("キック角度の最低要求精度[deg]", 1.0);
+    kick_skill.setParameter("kick_power", 0.8);
+    kick_skill.setParameter("chip_kick", false);
+    kick_skill.setParameter("with_dribble", false);
+    kick_skill.setParameter("dot_threshold", 0.95);
   }
 
   Status update(const ConsaiVisualizerWrapper::SharedPtr & visualizer) override
@@ -52,38 +58,13 @@ public:
     }
 
     Point target = world_model()->ball.pos + getNormVec(best_angle) * 0.5;
-
-    {  // この部分はいずれ回り込み＆キックのスキルとして一般化したい
-       // (その時は動くボールへの回り込みを含めて)
-      // パラメータ候補：キックパワー・dotしきい値・角度しきい値・経由ポイント距離・突撃速度
-      auto ball_pos = world_model()->ball.pos;
-      double dot =
-        (robot()->pose.pos - ball_pos).normalized().dot((ball_pos - target).normalized());
-      if (
-        (dot > 0.95 || (robot()->pose.pos - ball_pos).norm() > 0.1) &&
-        std::abs(getAngleDiff(getAngle(target - world_model()->ball.pos), robot()->pose.theta)) >
-          0.1) {
-        // キック
-        command.setTargetPosition(ball_pos + (target - ball_pos).normalized() * 0.5)
-          .kickStraight(0.8)
-          .disableCollisionAvoidance()
-          .enableCollisionAvoidance()
-          .disableBallAvoidance();
-      } else {
-        // 経由ポイントへGO
-        Point intermediate_point = ball_pos + (ball_pos - target).normalized() * 0.3;
-        command.setTargetPosition(intermediate_point)
-          .enableCollisionAvoidance()
-          .enableBallAvoidance();
-      }
-      // 共通コマンド
-      command.liftUpDribbler().setTargetTheta(getAngle(target - world_model()->ball.pos));
-    }
-
-    return Status::RUNNING;
+    kick_skill.setParameter("target", target);
+    return kick_skill.run(visualizer);
   }
 
   void print(std::ostream & os) const override { os << "[GoalKick] "; }
+
+  Kick kick_skill;
 };
 }  // namespace crane::skills
 #endif  // CRANE_ROBOT_SKILLS__GOAL_KICK_HPP_
