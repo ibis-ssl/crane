7290456327a88013c2208950af2f3eaa3166dcd5
Kotaro Yoshimoto <pythagora.yoshimoto@gmail.com>
2024-07-30 07:42:09 +0900

    GoalKickスキルのシュート角度計算部分をstatic関数化

--- crane_robot_skills/include/crane_robot_skills/goal_kick.hpp
+++ crane_robot_skills/include/crane_robot_skills/goal_kick.hpp
@@ -29,33 +29,8 @@ public:
 
   Status update(const ConsaiVisualizerWrapper::SharedPtr & visualizer) override
   {
-    auto [best_angle, goal_angle_width] =
-      world_model()->getLargestGoalAngleRangeFromPoint(world_model()->ball.pos);
-    // 隙間のなかで更に良い角度を計算する。
-    // キック角度の最低要求精度をオフセットとしてできるだけ端っこを狙う
-    double minimum_angle_accuracy =
-      getParameter<double>("キック角度の最低要求精度[deg]") * M_PI / 180.0;
-    if (goal_angle_width < minimum_angle_accuracy * 2.0) {
-      {
-        double best_angle1, best_angle2;
-        best_angle1 = best_angle - goal_angle_width / 2.0 + minimum_angle_accuracy;
-        best_angle2 = best_angle + goal_angle_width / 2.0 - minimum_angle_accuracy;
-        Point their_goalie_pos =
-          world_model()
-            ->getNearestRobotsWithDistanceFromPoint(
-              world_model()->getTheirGoalCenter(), world_model()->theirs.getAvailableRobots())
-            .first->pose.pos;
-        double their_goalie_angle = getAngle(their_goalie_pos - world_model()->ball.pos);
-        // 敵ゴールキーパーから角度差が大きい方を選択
-        if (
-          std::abs(getAngleDiff(their_goalie_angle, best_angle1)) <
-          std::abs(getAngleDiff(their_goalie_angle, best_angle2))) {
-          best_angle = best_angle2;
-        } else {
-          best_angle = best_angle1;
-        }
-      }
-    }
+    double best_angle = getBestAngleToShootFromBall(
+      getParameter<double>("キック角度の最低要求精度[deg]") * M_PI / 180., world_model());
 
     Point target = world_model()->ball.pos + getNormVec(best_angle) * 0.5;
     kick_skill.setParameter("target", target);
@@ -65,6 +40,35 @@ public:
   void print(std::ostream & os) const override { os << "[GoalKick] "; }
 
   Kick kick_skill;
+
+  static double getBestAngleToShootFromBall(
+    double minimum_angle_accuracy, const WorldModelWrapper::SharedPtr & world_model)
+  {
+    auto [best_angle, goal_angle_width] =
+      world_model->getLargestGoalAngleRangeFromPoint(world_model->ball.pos);
+    // 隙間のなかで更に良い角度を計算する。
+    // キック角度の最低要求精度をオフセットとしてできるだけ端っこを狙う
+    if (goal_angle_width < minimum_angle_accuracy * 2.0) {
+      double best_angle1, best_angle2;
+      best_angle1 = best_angle - goal_angle_width / 2.0 + minimum_angle_accuracy;
+      best_angle2 = best_angle + goal_angle_width / 2.0 - minimum_angle_accuracy;
+      Point their_goalie_pos =
+        world_model
+          ->getNearestRobotsWithDistanceFromPoint(
+            world_model->getTheirGoalCenter(), world_model->theirs.getAvailableRobots())
+          .first->pose.pos;
+      double their_goalie_angle = getAngle(their_goalie_pos - world_model->ball.pos);
+      // 敵ゴールキーパーから角度差が大きい方を選択
+      if (
+        std::abs(getAngleDiff(their_goalie_angle, best_angle1)) <
+        std::abs(getAngleDiff(their_goalie_angle, best_angle2))) {
+        best_angle = best_angle2;
+      } else {
+        best_angle = best_angle1;
+      }
+    }
+    return best_angle;
+  }
 };
 }  // namespace crane::skills
 #endif  // CRANE_ROBOT_SKILLS__GOAL_KICK_HPP_
