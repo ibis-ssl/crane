909155aee3a5f14d7e22373972141f69447b8035
Kotaro Yoshimoto <pythagora.yoshimoto@gmail.com>
2024-08-02 22:34:51 +0900

    BallNearByPositionerスキルのバグ修正

--- crane_robot_skills/include/crane_robot_skills/ball_nearby_positioner.hpp
+++ crane_robot_skills/include/crane_robot_skills/ball_nearby_positioner.hpp
@@ -49,36 +49,40 @@ public:
 
     distance_from_ball += getParameter<double>("margin_distance");
     double normalized_offset =
-      (getParameter<int>("total_robot_number") - getParameter<int>("robot_index") - 1) / 2.;
+      (getParameter<int>("total_robot_number") - getParameter<int>("current_robot_index") - 1) / 2.;
     double offset = normalized_offset * getParameter<double>("robot_interval");
-    Point base_position = world_model()->ball.pos + [&](const std::string & policy) {
-      if (policy == "goal") {
-        return (world_model()->getOurGoalCenter() - world_model()->ball.pos).normalized();
-      } else if (policy == "pass") {
-        // 2番目に近いロボット
-        auto theirs = world_model()->theirs.getAvailableRobots();
-        if (theirs.size() > 2) {
-          auto nearest_robot =
-            world_model()->getNearestRobotWithDistanceFromPoint(world_model()->ball.pos, theirs);
-          theirs.erase(
-            std::remove_if(
-              theirs.begin(), theirs.end(),
-              [&](const auto & r) { return r->id == nearest_robot.first->id; }),
-            theirs.end());
-          auto second_nearest_robot =
-            world_model()->getNearestRobotWithDistanceFromPoint(world_model()->ball.pos, theirs);
-          return (second_nearest_robot.first->pose.pos - world_model()->ball.pos).normalized();
+    Point base_position =
+      world_model()->ball.pos +
+      [&](const std::string & policy) {
+        if (policy == "goal") {
+          return (world_model()->getOurGoalCenter() - world_model()->ball.pos).normalized();
+        } else if (policy == "pass") {
+          // 2番目に近いロボット
+          auto theirs = world_model()->theirs.getAvailableRobots();
+          if (theirs.size() > 2) {
+            auto nearest_robot =
+              world_model()->getNearestRobotWithDistanceFromPoint(world_model()->ball.pos, theirs);
+            theirs.erase(
+              std::remove_if(
+                theirs.begin(), theirs.end(),
+                [&](const auto & r) { return r->id == nearest_robot.first->id; }),
+              theirs.end());
+            auto second_nearest_robot =
+              world_model()->getNearestRobotWithDistanceFromPoint(world_model()->ball.pos, theirs);
+            return (second_nearest_robot.first->pose.pos - world_model()->ball.pos).normalized();
+          } else {
+            throw std::runtime_error(
+              "[BallNearByPositioner] 「positioning policy: "
+              "pass」の計算には少なくとも2台の敵ロボットが必要です");
+          }
         } else {
           throw std::runtime_error(
-            "[BallNearByPositioner] 「positioning policy: "
-            "pass」の計算には少なくとも2台の敵ロボットが必要です");
+            "[BallNearByPositioner] "
+            "予期しないパラメータ「positioning_policy」が入力されています: " +
+            policy);
         }
-      } else {
-        throw std::runtime_error(
-          "[BallNearByPositioner] 予期しないパラメータ「positioning policy」が入力されています: " +
-          policy);
-      }
-    }(getParameter<std::string>("line_policy")) * distance_from_ball;
+      }(getParameter<std::string>("positioning_policy")) *
+        distance_from_ball;
 
     Point target_position = [&](const std::string & policy) -> Point {
       Vector2 ball_to_base = (base_position - world_model()->ball.pos);
@@ -92,7 +96,7 @@ public:
         return base_position + ball_to_base + getVerticalVec(ball_to_base.normalized()) * offset;
       } else {
         throw std::runtime_error(
-          "[BallNearByPositioner] 予期しないパラメータ「line policy」が入力されています: " +
+          "[BallNearByPositioner] 予期しないパラメータ「line_policy」が入力されています: " +
           policy);
       }
     }(getParameter<std::string>("line_policy"));
