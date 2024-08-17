bb04fa5478734d74f560846826021f4a15e3fa95
Kotaro Yoshimoto <pythagora.yoshimoto@gmail.com>
2024-08-17 18:01:03 +0900

    コメントヒントを削除

--- consai_ros2/consai_vision_tracker/src/visualization_data_handler.cpp
+++ consai_ros2/consai_vision_tracker/src/visualization_data_handler.cpp
@@ -279,216 +279,6 @@ TrackedFrame::UniquePtr VisualizationDataHandler::publish_vis_tracked(TrackedFra
   return msg;
 }
 
-/*
-def vis_info(referee: Referee, blue_bots: int, yellow_bots: int,
-             placement_pos: State2D):
-    # レフェリー情報を描画オブジェクトに変換する
-    MARGIN_X = 0.02
-    TEXT_HEIGHT = 0.05
-    STAGE_COMMAND_WIDTH = 0.15
-    STAGE_COMMAND_X = 0.0 + MARGIN_X
-    TIMER_WIDTH = 0.15
-    TIMER_X = STAGE_COMMAND_X + STAGE_COMMAND_WIDTH + MARGIN_X
-    BOTS_WIDTH = 0.2
-    BOTS_X = TIMER_X + TIMER_WIDTH + MARGIN_X
-    CARDS_WIDTH = 0.1
-    CARDS_X = BOTS_X + BOTS_WIDTH + MARGIN_X
-    YELLOW_CARD_TIMES_WIDTH = 0.1
-    YELLOW_CARD_TIMES_X = CARDS_X + CARDS_WIDTH + MARGIN_X
-    TIMEOUT_WIDTH = 0.1
-    TIMEOUT_X = YELLOW_CARD_TIMES_X + YELLOW_CARD_TIMES_WIDTH + MARGIN_X
-    COLOR_TEXT_BLUE = 'deepskyblue'
-    COLOR_TEXT_YELLOW = 'yellow'
-    COLOR_TEXT_WARNING = 'red'
-
-    vis_objects = Objects()
-    vis_objects.layer = 'referee'
-    vis_objects.sub_layer = 'info'
-    vis_objects.z_order = 2
-
-    # 左端にSTAGEとCOMMANDを表示
-    vis_annotation = ShapeAnnotation()
-    vis_annotation.text = parse_stage(referee.stage)
-    vis_annotation.color.name = 'white'
-    vis_annotation.normalized_x = STAGE_COMMAND_X
-    vis_annotation.normalized_y = 0.0
-    vis_annotation.normalized_width = STAGE_COMMAND_WIDTH
-    vis_annotation.normalized_height = TEXT_HEIGHT
-    vis_objects.annotations.append(vis_annotation)
-
-    vis_annotation = ShapeAnnotation()
-    vis_annotation.text, vis_annotation.color.name = parse_command(
-        referee, COLOR_TEXT_BLUE, COLOR_TEXT_YELLOW)
-    vis_annotation.normalized_x = STAGE_COMMAND_X
-    vis_annotation.normalized_y = TEXT_HEIGHT
-    vis_annotation.normalized_width = STAGE_COMMAND_WIDTH
-    vis_annotation.normalized_height = TEXT_HEIGHT
-    vis_objects.annotations.append(vis_annotation)
-
-    # 残り時間とACT_TIMEを表示
-    if referee.stage_time_left:
-        vis_annotation = ShapeAnnotation()
-        vis_annotation.text = parse_stage_time_left(referee.stage_time_left[0])
-        vis_annotation.color.name = 'white'
-        vis_annotation.normalized_x = TIMER_X
-        vis_annotation.normalized_y = 0.0
-        vis_annotation.normalized_width = TIMER_WIDTH
-        vis_annotation.normalized_height = TEXT_HEIGHT
-        vis_objects.annotations.append(vis_annotation)
-
-    if referee.current_action_time_remaining:
-        vis_annotation = ShapeAnnotation()
-        vis_annotation.text = parse_action_time_remaining(referee.current_action_time_remaining[0])
-        vis_annotation.color.name = 'white'
-        vis_annotation.normalized_x = TIMER_X
-        vis_annotation.normalized_y = TEXT_HEIGHT
-        vis_annotation.normalized_width = TIMER_WIDTH
-        vis_annotation.normalized_height = TEXT_HEIGHT
-        vis_objects.annotations.append(vis_annotation)
-
-    # ロボット数
-    if referee.blue.max_allowed_bots:
-        vis_annotation = ShapeAnnotation()
-        vis_annotation.color.name = COLOR_TEXT_BLUE
-        # 許可台数よりロボットが多い場合は色を変える
-        if blue_bots > referee.blue.max_allowed_bots[0]:
-            vis_annotation.color.name = COLOR_TEXT_WARNING
-        vis_annotation.text = 'BLUE BOTS: {}/{}'.format(
-            blue_bots, referee.blue.max_allowed_bots[0])
-        vis_annotation.normalized_x = BOTS_X
-        vis_annotation.normalized_y = 0.0
-        vis_annotation.normalized_width = BOTS_WIDTH
-        vis_annotation.normalized_height = TEXT_HEIGHT
-        vis_objects.annotations.append(vis_annotation)
-
-    if referee.yellow.max_allowed_bots:
-        vis_annotation = ShapeAnnotation()
-        vis_annotation.color.name = COLOR_TEXT_YELLOW
-        # 許可台数よりロボットが多い場合は色を変える
-        if yellow_bots > referee.yellow.max_allowed_bots[0]:
-            vis_annotation.color.name = COLOR_TEXT_WARNING
-        vis_annotation.text = 'YELLOW BOTS: {}/{}'.format(
-            yellow_bots, referee.yellow.max_allowed_bots[0])
-        vis_annotation.normalized_x = BOTS_X
-        vis_annotation.normalized_y = TEXT_HEIGHT
-        vis_annotation.normalized_width = BOTS_WIDTH
-        vis_annotation.normalized_height = TEXT_HEIGHT
-        vis_objects.annotations.append(vis_annotation)
-
-    # カード数
-    vis_annotation = ShapeAnnotation()
-    vis_annotation.color.name = COLOR_TEXT_BLUE
-    vis_annotation.text = 'R: {}, Y:{}'.format(
-        referee.blue.red_cards,
-        referee.blue.yellow_cards)
-    vis_annotation.normalized_x = CARDS_X
-    vis_annotation.normalized_y = 0.0
-    vis_annotation.normalized_width = CARDS_WIDTH
-    vis_annotation.normalized_height = TEXT_HEIGHT
-    vis_objects.annotations.append(vis_annotation)
-
-    vis_annotation = ShapeAnnotation()
-    vis_annotation.color.name = COLOR_TEXT_YELLOW
-    vis_annotation.text = 'R: {}, Y:{}'.format(
-        referee.yellow.red_cards,
-        referee.yellow.yellow_cards)
-    vis_annotation.normalized_x = CARDS_X
-    vis_annotation.normalized_y = TEXT_HEIGHT
-    vis_annotation.normalized_width = CARDS_WIDTH
-    vis_annotation.normalized_height = TEXT_HEIGHT
-    vis_objects.annotations.append(vis_annotation)
-
-    # イエローカードの時間
-    vis_annotation = ShapeAnnotation()
-    vis_annotation.color.name = COLOR_TEXT_BLUE
-    vis_annotation.text = parse_yellow_card_times(referee.blue.yellow_card_times)
-    vis_annotation.normalized_x = YELLOW_CARD_TIMES_X
-    vis_annotation.normalized_y = 0.0
-    vis_annotation.normalized_width = YELLOW_CARD_TIMES_WIDTH
-    vis_annotation.normalized_height = TEXT_HEIGHT
-    vis_objects.annotations.append(vis_annotation)
-
-    vis_annotation = ShapeAnnotation()
-    vis_annotation.color.name = COLOR_TEXT_YELLOW
-    vis_annotation.text = parse_yellow_card_times(referee.yellow.yellow_card_times)
-    vis_annotation.normalized_x = YELLOW_CARD_TIMES_X
-    vis_annotation.normalized_y = TEXT_HEIGHT
-    vis_annotation.normalized_width = YELLOW_CARD_TIMES_WIDTH
-    vis_annotation.normalized_height = TEXT_HEIGHT
-    vis_objects.annotations.append(vis_annotation)
-
-    # タイムアウト
-    vis_annotation = ShapeAnnotation()
-    vis_annotation.color.name = COLOR_TEXT_BLUE
-    vis_annotation.text = parse_timeouts(referee.blue.timeouts, referee.blue.timeout_time)
-    vis_annotation.normalized_x = TIMEOUT_X
-    vis_annotation.normalized_y = 0.0
-    vis_annotation.normalized_width = TIMEOUT_WIDTH
-    vis_annotation.normalized_height = TEXT_HEIGHT
-    vis_objects.annotations.append(vis_annotation)
-
-    vis_annotation = ShapeAnnotation()
-    vis_annotation.color.name = COLOR_TEXT_YELLOW
-    vis_annotation.text = parse_timeouts(referee.yellow.timeouts, referee.yellow.timeout_time)
-    vis_annotation.normalized_x = TIMEOUT_X
-    vis_annotation.normalized_y = TEXT_HEIGHT
-    vis_annotation.normalized_width = TIMEOUT_WIDTH
-    vis_annotation.normalized_height = TEXT_HEIGHT
-    vis_objects.annotations.append(vis_annotation)
-
-    # プレースメント位置
-    if referee.command == Referee.COMMAND_BALL_PLACEMENT_BLUE or \
-       referee.command == Referee.COMMAND_BALL_PLACEMENT_YELLOW:
-        vis_circle = ShapeCircle()
-        vis_circle.center.x = placement_pos.x
-        vis_circle.center.y = placement_pos.y
-        vis_circle.radius = 0.15
-        vis_circle.line_color.name = 'aquamarine'
-        vis_circle.fill_color.name = 'aquamarine'
-        vis_circle.line_size = 1
-        vis_circle.caption = 'placement pos'
-        vis_objects.circles.append(vis_circle)
-
-    return vis_objects
- */
-
-/*
-def parse_stage(ref_stage):
-    # レフェリーステージを文字列に変換する
-    output = "STAGE"
-
-    if ref_stage == Referee.STAGE_NORMAL_FIRST_HALF_PRE:
-        output = "FIRST HALF PRE"
-    elif ref_stage == Referee.STAGE_NORMAL_FIRST_HALF:
-        output = "FIRST HALF"
-    elif ref_stage == Referee.STAGE_NORMAL_HALF_TIME:
-        output = "HALF TIME"
-    elif ref_stage == Referee.STAGE_NORMAL_SECOND_HALF_PRE:
-        output = "SECOND HALF PRE"
-    elif ref_stage == Referee.STAGE_NORMAL_SECOND_HALF:
-        output = "SECOND HALF"
-    elif ref_stage == Referee.STAGE_EXTRA_TIME_BREAK:
-        output = "EX TIME BREAK"
-    elif ref_stage == Referee.STAGE_EXTRA_FIRST_HALF_PRE:
-        output = "EX FIRST HALF PRE"
-    elif ref_stage == Referee.STAGE_EXTRA_FIRST_HALF:
-        output = "EX FIRST HALF"
-    elif ref_stage == Referee.STAGE_EXTRA_HALF_TIME:
-        output = "EX HALF TIME"
-    elif ref_stage == Referee.STAGE_EXTRA_SECOND_HALF_PRE:
-        output = "EX SECOND HALF PRE"
-    elif ref_stage == Referee.STAGE_EXTRA_SECOND_HALF:
-        output = "EX SECOND HALF"
-    elif ref_stage == Referee.STAGE_PENALTY_SHOOTOUT_BREAK:
-        output = "PENALTY SHOOTOUT BREAK"
-    elif ref_stage == Referee.STAGE_PENALTY_SHOOTOUT:
-        output = "PENALTY SHOOTOUT"
-    elif ref_stage == Referee.STAGE_POST_GAME:
-        output = "POST_GAME"
-
-    return output
- */
-
 auto parse_stage = [](const auto & ref_stage) -> std::string {
   std::string output = "STAGE";
 
@@ -524,70 +314,6 @@ auto parse_stage = [](const auto & ref_stage) -> std::string {
 
   return output;
 };
-
-/*
-def parse_command(referee: Referee,
-                  blue_color: str = 'blue', yellow_color: str = 'yellow') -> (str, str):
-    # レフェリーコマンドを文字列と文字色に変換する
-    output = "COMMAND"
-    text_color = 'white'
-
-    if len(referee.designated_position) > 0:
-        placement_pos_x = referee.designated_position[0].x * 0.001
-        placement_pos_y = referee.designated_position[0].y * 0.001
-
-    ref_command = referee.command
-
-    if ref_command == Referee.COMMAND_HALT:
-        output = "HALT"
-    elif ref_command == Referee.COMMAND_STOP:
-        output = "STOP"
-    elif ref_command == Referee.COMMAND_NORMAL_START:
-        output = "NORMAL START"
-    elif ref_command == Referee.COMMAND_FORCE_START:
-        output = "FORCE START"
-    elif ref_command == Referee.COMMAND_PREPARE_KICKOFF_YELLOW:
-        output = "PREPARE KICK OFF YELLOW"
-        text_color = yellow_color
-    elif ref_command == Referee.COMMAND_PREPARE_KICKOFF_BLUE:
-        output = "PREPARE KICK OFF BLUE"
-        text_color = blue_color
-    elif ref_command == Referee.COMMAND_PREPARE_PENALTY_YELLOW:
-        output = "PREPARE PENALTY YELLOW"
-        text_color = yellow_color
-    elif ref_command == Referee.COMMAND_PREPARE_PENALTY_BLUE:
-        output = "PREPARE PENALTY BLUE"
-        text_color = blue_color
-    elif ref_command == Referee.COMMAND_DIRECT_FREE_YELLOW:
-        output = "DIRECT FREE YELLOW"
-        text_color = yellow_color
-    elif ref_command == Referee.COMMAND_DIRECT_FREE_BLUE:
-        output = "DIRECT FREE BLUE"
-        text_color = blue_color
-    elif ref_command == Referee.COMMAND_INDIRECT_FREE_YELLOW:
-        output = "INDIRECT FREE YELLOW"
-        text_color = yellow_color
-    elif ref_command == Referee.COMMAND_INDIRECT_FREE_BLUE:
-        output = "INDIRECT FREE BLUE"
-        text_color = blue_color
-    elif ref_command == Referee.COMMAND_TIMEOUT_YELLOW:
-        output = "TIMEOUT YELLOW"
-        text_color = yellow_color
-    elif ref_command == Referee.COMMAND_TIMEOUT_BLUE:
-        output = "TIMEOUT BLUE"
-        text_color = blue_color
-    elif ref_command == Referee.COMMAND_BALL_PLACEMENT_YELLOW:
-        output = "BALL PLACEMENT YELLOW"
-        output += "(x: {:.1f}, y: {:.1f})".format(placement_pos_x, placement_pos_y)
-        text_color = yellow_color
-    elif ref_command == Referee.COMMAND_BALL_PLACEMENT_BLUE:
-        output = "BALL PLACEMENT BLUE"
-        output += "(x: {:.1f}, y: {:.1f})".format(placement_pos_x, placement_pos_y)
-        text_color = blue_color
-
-    return (output, text_color)
-
- */
 
 auto parse_command = [](
                        const Referee referee, std::string blue_color = "blue",
@@ -673,7 +399,6 @@ auto parse_command = [](
 
 void VisualizationDataHandler::publish_vis_referee(const Referee::SharedPtr msg)
 {
-  // std::cout << "publish_vis_referee" << std::endl;
   // レフェリー情報を描画オブジェクトに変換してpublishする
   const double MARGIN_X = 0.02;
   const double TEXT_HEIGHT = 0.05;
@@ -740,19 +465,6 @@ void VisualizationDataHandler::publish_vis_referee(const Referee::SharedPtr msg)
   }
 
   if (msg->current_action_time_remaining.size() > 0) {
-    /*
-    def _microseconds_to_text(microseconds):
-    minutes, seconds = divmod(math.ceil(microseconds * 1e-6), 60)  # ceilで小数点切り上げ
-    return '{} : {:0=2}'.format(minutes, seconds)  # 秒はゼロで埋める
-
-    def parse_action_time_remaining(ref_action_time_remaining):
-    # アクション残り時間(usec)を文字列に変換する
-    text = "0:00"
-    if ref_action_time_remaining > 0:
-        text = _microseconds_to_text(ref_action_time_remaining)
-    return "ACT: " + text
-    */
-
     auto parse_action_time_remaining = [](const int ref_action_time_remaining) {
       auto parse_microseconds_to_text = [](const auto & microseconds) {
         auto [minutes, seconds] = std::div(std::ceil(microseconds * 1e-6), 60);
@@ -810,21 +522,6 @@ void VisualizationDataHandler::publish_vis_referee(const Referee::SharedPtr msg)
   vis_objects->annotations.push_back(vis_annotation);
 
   // イエローカードの時間
-
-  /*
-  def parse_yellow_card_times(yellow_card_times):
-    if len(yellow_card_times) == 0:
-        return "NO CARDS"
-
-    text = ""
-    for i in range(len(yellow_card_times)):
-        text += _microseconds_to_text(yellow_card_times[i])
-        if i != len(yellow_card_times) - 1:
-            text += "\n"
-
-    return text
-  */
-
   auto parse_yellow_card_times = [](const auto & yellow_card_times) {
     auto parse_microseconds_to_text = [](const auto & microseconds) {
       auto [minutes, seconds] = std::div(std::ceil(microseconds * 1e-6), 60);
@@ -857,13 +554,6 @@ void VisualizationDataHandler::publish_vis_referee(const Referee::SharedPtr msg)
   vis_objects->annotations.push_back(vis_annotation);
 
   // タイムアウト
-
-  /*
-  def parse_timeouts(timeouts, timeout_time):
-    return 'Timeouts: {}\n {}'.format(
-        timeouts, _microseconds_to_text(timeout_time))
-  */
-
   auto parse_timeouts = [](const auto & timeouts, const auto & timeout_time) {
     return "Timeouts: " + std::to_string(timeouts) + "\n" + std::to_string(timeout_time);
   };
