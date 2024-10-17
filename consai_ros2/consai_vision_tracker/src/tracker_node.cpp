3450caa9dcceb3d25bc2287ae6386e78349b1642
Kotaro Yoshimoto <pythagora.yoshimoto@gmail.com>
2024-10-18 00:27:50 +0900

    VisionTrackerは最小限に

--- consai_ros2/consai_vision_tracker/src/tracker_node.cpp
+++ consai_ros2/consai_vision_tracker/src/tracker_node.cpp
@@ -13,7 +13,7 @@ int main(int argc, char * argv[])
   rclcpp::init(argc, argv);
   rclcpp::executors::SingleThreadedExecutor exe;
   rclcpp::NodeOptions options;
-  auto node = std::make_shared<consai_vision_tracker::Tracker>(options);
+  auto node = std::make_shared<consai_vision_tracker::VisionVisualizer>(options);
   exe.add_node(node->get_node_base_interface());
   exe.spin();
   rclcpp::shutdown();
