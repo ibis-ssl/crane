633da13fe3b8e35536411f632cab04de9543cc00
Kotaro Yoshimoto <pythagora.yoshimoto@gmail.com>
2024-12-02 22:36:28 +0900

    進行中のキックの管理

--- crane_game_analyzer/include/crane_game_analyzer/kick_event_detector.hpp
+++ crane_game_analyzer/include/crane_game_analyzer/kick_event_detector.hpp
@@ -41,11 +41,56 @@ public:
     detected_bots = filterByBotAngle(0.5, detected_bots, world_model);
     detected_bots = filterByDistanceIncrease(detected_bots, world_model);
     // print detected bots
+    std::optional<Point> kick_event_origin = std::nullopt;
     for (const auto & id : detected_bots.friends) {
       RCLCPP_INFO_STREAM(rclcpp::get_logger("aaaa"), "Detected friend: " << static_cast<int>(id));
+      visualizer->addCircle(
+        world_model->getOurRobot(id)->pose.pos, 0.5, 2, "blue", "blue", 1.0, "KICK");
+      kick_event_origin = world_model->ball.pos;
     }
     for (const auto & id : detected_bots.enemies) {
       RCLCPP_INFO_STREAM(rclcpp::get_logger("aaaa"), "Detected enemy: " << static_cast<int>(id));
+      visualizer->addCircle(
+        world_model->getTheirRobot(id)->pose.pos, 0.5, 2, "blue", "blue", 1.0, "KICK");
+      kick_event_origin = world_model->ball.pos;
+    }
+
+    // キック中断判定
+    if (ongoing_kick_origin && hasInterruptedOngoningKick(world_model)) {
+      ongoing_kick_origin = std::nullopt;
+    }
+
+    if (kick_event_origin) {
+      ongoing_kick_origin = kick_event_origin;
+    }
+
+    if (kick_event_origin) {
+      visualizer->addLine(
+        world_model->ball.pos, ongoing_kick_origin.value(), 2, "red", 1.0, "KICK");
+    }
+
+    for (const auto & record : records) {
+      visualizer->addCircle(record.position, 0.1, 2, "red", "", 1.0, "ball");
+    }
+  }
+
+  bool hasInterruptedOngoningKick(const WorldModelWrapper::UniquePtr & world_model) const
+  {
+    // 進行中のKickが中断されたらtrueを返す
+    if (not ongoing_kick_origin.has_value()) {
+      // 進行中のKickがそもそもない
+      return false;
+    } else {
+      const auto & latest_ball = world_model->ball;
+      const auto ball_vel_normed = latest_ball.vel.normalized();
+      for (const auto & record : records) {
+        if (
+          (record.position - ongoing_kick_origin.value()).normalized().dot(ball_vel_normed) < 0.7) {
+          // 進行方向がおかしいので中断されたと判断
+          return true;
+        }
+      }
+      return false;
     }
   }
 
@@ -57,19 +102,36 @@ public:
   {
     using ranges::views::filter;
     DetectedBots detected_bots;
-    detected_bots.friends =
-      available_bots.friends | filter([&](const auto & id) {
-        auto distance = (records.front().position - world_model->getOurRobot(id)->pose.pos).norm();
-        return distance < threshold;
-      }) |
-      ranges::to<std::vector>();
-    detected_bots.enemies =
-      available_bots.enemies | filter([&](const auto & id) {
-        auto distance =
-          (records.front().position - world_model->getTheirRobot(id)->pose.pos).norm();
-        return distance < threshold;
-      }) |
-      ranges::to<std::vector>();
+    detected_bots.friends = available_bots.friends | filter([&](const auto & id) {
+                              auto robot_pos = world_model->getOurRobot(id)->pose.pos;
+                              auto oldest_distance = (records.front().position - robot_pos).norm();
+                              if (oldest_distance > threshold) {
+                                return false;
+                              }
+                              for (const auto & record : records) {
+                                auto distance = (record.position - robot_pos).norm();
+                                if (distance < oldest_distance) {
+                                  return false;
+                                }
+                              }
+                              return true;
+                            }) |
+                            ranges::to<std::vector>();
+    detected_bots.enemies = available_bots.enemies | filter([&](const auto & id) {
+                              auto robot_pos = world_model->getTheirRobot(id)->pose.pos;
+                              auto oldest_distance = (records.front().position - robot_pos).norm();
+                              if (oldest_distance > threshold) {
+                                return false;
+                              }
+                              for (const auto & record : records) {
+                                auto distance = (record.position - robot_pos).norm();
+                                if (distance < oldest_distance) {
+                                  return false;
+                                }
+                              }
+                              return true;
+                            }) |
+                            ranges::to<std::vector>();
     return detected_bots;
   }
 
@@ -83,7 +145,7 @@ public:
                           }) |
                           ranges::to<std::vector>();
 
-    if (faster_records.empty()) {
+    if (not faster_records.empty()) {
       return available_bots;
     } else {
       return DetectedBots();
@@ -98,13 +160,13 @@ public:
     DetectedBots detected_bots;
     detected_bots.friends = available_bots.friends | ranges::views::filter([&](const auto & id) {
                               auto robot_pose = world_model->getOurRobot(id)->pose;
-                              auto ball_angle = getAngle(records.front().position - robot_pose.pos);
+                              auto ball_angle = getAngle(records.back().position - robot_pose.pos);
                               return getAngleDiff(ball_angle, robot_pose.theta) < threshold;
                             }) |
                             ranges::to<std::vector>();
     detected_bots.enemies = available_bots.enemies | ranges::views::filter([&](const auto & id) {
                               auto robot_pose = world_model->getTheirRobot(id)->pose;
-                              auto ball_angle = getAngle(records.front().position - robot_pose.pos);
+                              auto ball_angle = getAngle(records.back().position - robot_pose.pos);
                               return getAngleDiff(ball_angle, robot_pose.theta) < threshold;
                             }) |
                             ranges::to<std::vector>();
@@ -153,7 +215,12 @@ public:
   };
 
 private:
-  std::queue<Record> records;
+  std::deque<Record> records;
+
+  std::optional<Point> ongoing_kick_origin = std::nullopt;
+
+  std::deque<std::pair<Point, Point>> kick_history;
+
   static constexpr int QUEUE_SIZE = 10;
 
   double distance_threshold = 0.15;
