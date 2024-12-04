7c65402310fae0e5283ed09b7ac7c334ae72996e
Kotaro Yoshimoto <pythagora.yoshimoto@gmail.com>
2024-12-05 00:57:55 +0900

    cspellのエラーを修正

--- crane_game_analyzer/include/crane_game_analyzer/kick_event_detector.hpp
+++ crane_game_analyzer/include/crane_game_analyzer/kick_event_detector.hpp
@@ -107,7 +107,7 @@ public:
     }
   }
 
-  bool hasInterruptedOngoningKick(const WorldModelWrapper::UniquePtr & world_model) const
+  bool hasInterruptedOnGoningKick(const WorldModelWrapper::UniquePtr & world_model) const
   {
     // 進行中のKickが中断されたらtrueを返す
     if (not ongoing_kick_origin.has_value()) {
