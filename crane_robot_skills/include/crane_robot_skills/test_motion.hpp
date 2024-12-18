0be50374e2658e759ce8b854802851e59ae1c16a
Kotaro Yoshimoto <pythagora.yoshimoto@gmail.com>
2024-12-19 07:55:48 +0900

    モーションを登録

--- crane_robot_skills/include/crane_robot_skills/test_motion.hpp
+++ crane_robot_skills/include/crane_robot_skills/test_motion.hpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2023 ibis-ssl
+// Copyright (c) 2024 ibis-ssl
 //
 // Use of this source code is governed by an MIT-style
 // license that can be found in the LICENSE file or at
@@ -15,29 +15,72 @@ namespace crane::skills
 {
 class TestMotionPosition : public SkillBase<RobotCommandWrapperPosition>
 {
+private:
+  int current_section = 0;
+  rclcpp::Time latest_section_start_time;
+  rclcpp::Clock clock;
+  std::unordered_map<std::string, std::function<Point(const int, const double)>> motion_functions;
 public:
   explicit TestMotionPosition(RobotCommandWrapperBase::SharedPtr & base)
-  : SkillBase("TestMotion", base)
+  : SkillBase("TestMotion", base), clock(RCL_ROS_TIME)
   {
-    setParameter("motion", "一文字");
+    // segment / square / circle
+    setParameter("motion", "segment");
     setParameter("origin", Point::Zero());
     setParameter("section_time", 5.);
     setParameter("sleep_time", 1.);
     setParameter("distance", 1.);
+    latest_section_start_time = clock.now();
+    motion_functions["segment"] = [&](const int section, const double parameter) {
+      return getPositionSegment(section, parameter);
+    };
+    motion_functions["square"] = [&](const int section, const double parameter) {
+      return getPositionSquare(section, parameter);
+    };
+    motion_functions["circle"] = [&](const int section, const double parameter) {
+      return getPositionCircle(section, parameter);
+    };
+  }
+
+  void reset(const std::string & motion)
+  {
+    setParameter("motion", motion);
+    current_section = 0;
+    latest_section_start_time = clock.now();
   }
 
   Status update() override
   {
-    command.stopHere();
+    const std::string motion_name = getParameter<std::string>("motion");
+    const double section_time = getParameter<double>("section_time");
+    const double sleep_time = getParameter<double>("sleep_time");
+    if (auto motion = motion_functions.find(motion_name); motion != motion_functions.end()) {
+      if ( clock.now() - latest_section_start_time >= rclcpp::Duration::from_seconds(section_time + sleep_time)) {
+        latest_section_start_time = clock.now();
+        current_section++;
+      }
+      const double parameter = (clock.now() - latest_section_start_time).seconds() / section_time;
+      const Point position = motion->second(current_section, parameter);
+      command.setTargetPosition(position);
+    }else {
+      std::stringstream what;
+      what << "TestMotionPositionでサポートされていないモーション \"" << motion_name << "\"が指定されています。";
+      what << "有効なのは [";
+      for (const auto & motion : motion_functions) {
+        what << motion.first << ", ";
+      }
+      what << "] です。";
+      throw std::runtime_error(what.str());
+    }
     return Status::RUNNING;
   }
 
   // 往復
-  Point getPositionA(const int section, const double parameter) const
+  Point getPositionSegment(const int section, const double parameter) const
   {
     const double distance = getParameter<double>("distance");
     const Point origin = getParameter<Point>("origin");
-    switch (section) {
+    switch (section % 2) {
       case 0:
         return origin + Point(distance * (parameter - 0.5), 0);
       case 1:
@@ -48,7 +91,7 @@ public:
   }
 
   // 四角
-  Point getPositionB(const int section, const double parameter) const
+  Point getPositionSquare(const int section, const double parameter) const
   {
     const double distance = getParameter<double>("distance");
     const Point origin = getParameter<Point>("origin");
@@ -56,7 +99,7 @@ public:
     const double up = distance * (parameter - 0.5);
     const double down = distance * (0.5 - parameter);
 
-    switch (section) {
+    switch (section % 4) {
       case 0:
         // x : minus -> plus, y: plus
         return origin + Point(up, distance * 0.5);
@@ -75,7 +118,7 @@ public:
   }
 
   // 円
-  Point getPositionC(const double parameter) const
+  Point getPositionCircle([[maybe_unused]] const int section, const double parameter) const
   {
     const double distance = getParameter<double>("distance");
     const Point origin = getParameter<Point>("origin");
