a709ea1aa956b714b4a44676e04551d17653ac01
Kotaro Yoshimoto <pythagora.yoshimoto@gmail.com>
2025-01-13 14:53:59 +0900

    キックの調整

--- crane_robot_skills/src/attacker.cpp
+++ crane_robot_skills/src/attacker.cpp
@@ -60,7 +60,6 @@ Attacker::Attacker(RobotCommandWrapperBase::SharedPtr & base)
         // パス
         command.disableBallAvoidance();
         kick_skill.setParameter("dot_threshold", 0.95);
-        kick_skill.setParameter("kick_power", 0.8);
         int receiver_id = getParameter<int>("receiver_id");
         if (receiver_id != -1) {
           kick_target = world_model()->getOurRobot(receiver_id)->pose.pos;
@@ -68,13 +67,24 @@ Attacker::Attacker(RobotCommandWrapperBase::SharedPtr & base)
         kick_skill.setParameter("target", kick_target);
         Segment kick_line{world_model()->ball.pos, kick_target};
         // 近くに敵ロボットがいればチップキック
+        bool chip_kick = false;
         if (const auto enemy_robots = world_model()->theirs.getAvailableRobots();
             not enemy_robots.empty()) {
           const auto & [nearest_enemy, enemy_distance] =
             world_model()->getNearestRobotWithDistanceFromSegment(kick_line, enemy_robots);
           if (enemy_distance < 0.4 && nearest_enemy->getDistance(world_model()->ball.pos) < 2.0) {
-            kick_skill.setParameter("kick_with_chip", true);
+            chip_kick = true;
           }
+        }
+        if (chip_kick) {
+          kick_skill.setParameter("kick_with_chip", true);
+          kick_skill.setParameter("kick_power", 0.9);
+          kick_skill.setParameter("with_dribble", true);
+          kick_skill.setParameter("dribble_power", 0.7);
+        } else {
+          kick_skill.setParameter("kick_power", 0.2);
+          kick_skill.setParameter("kick_with_chip", false);
+          kick_skill.setParameter("dribble_power", 0.0);
         }
         kick_skill.run();
         break;
@@ -290,7 +300,7 @@ Attacker::Attacker(RobotCommandWrapperBase::SharedPtr & base)
         kick_skill.setParameter("kick_with_chip", true);
       }
     }
-    kick_skill.setParameter("kick_power", 0.5);
+    kick_skill.setParameter("kick_power", 0.4);
     kick_skill.setParameter("dot_threshold", 0.97);
     return kick_skill.run();
   });
@@ -363,7 +373,8 @@ Attacker::Attacker(RobotCommandWrapperBase::SharedPtr & base)
   });
 
   addStateFunction(AttackerState::RECEIVE_BALL, [this]() -> Status {
-    receive_skill.setParameter("enable_redirect", true);
+    receive_skill.setParameter("enable_redirect", false);
+    receive_skill.setParameter("policy", std::string("closest"));
     receive_skill.setParameter("dribble_power", 0.0);
     receive_skill.setParameter("enable_software_bumper", false);
     return receive_skill.run();
