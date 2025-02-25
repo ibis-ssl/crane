656a4885fd0f257a3256f3ea5eee625813167c5d
Kotaro Yoshimoto <pythagora.yoshimoto@gmail.com>
2025-02-26 02:50:38 +0900

    パスの受けて

--- session/crane_planner_plugins/include/crane_planner_plugins/pass_receiver_planner.hpp
+++ session/crane_planner_plugins/include/crane_planner_plugins/pass_receiver_planner.hpp
@@ -9,7 +9,7 @@
 
 #include <algorithm>
 #include <crane_basics/boost_geometry.hpp>
-#include <crane_basics/interval.hpp>
+#include <crane_basics/ddps.hpp>
 #include <crane_msg_wrappers/robot_command_wrapper.hpp>
 #include <crane_msg_wrappers/world_model_wrapper.hpp>
 #include <crane_msgs/srv/robot_select.hpp>
@@ -47,8 +47,53 @@ public:
     const std::vector<RobotIdentifier> & robots, PlannerContext & context) override
   {
     if (receive_skill) {
-      auto command = receive_skill->getRobotCommand();
-      return {PlannerBase::Status::RUNNING, {command}};
+      if (world_model->ball.isMoving(1.0)) {
+        auto command = receive_skill->getRobotCommand();
+        return {PlannerBase::Status::RUNNING, {command}};
+      } else {
+        auto robot_pos = receive_skill->commander().getRobot()->pose.pos;
+        auto points = crane::getDPPSPoints(robot_pos, 0.1, 1.0, 16);
+        auto points_with_score =
+          points | ranges::views::filter([&](const Point & p) {
+            return world_model->point_checker.isFieldInside(p) &&
+                   not world_model->point_checker.isPenaltyArea(p);
+          }) |
+          ranges::views::filter([&](const Point & p) {
+            if (auto enemies = world_model->theirs.getAvailableRobots(); not enemies.empty()) {
+              return world_model->getNearestRobotWithDistanceFromSegment({robot_pos, p}, enemies)
+                       .second > 0.2;
+            } else {
+              return true;
+            }
+          }) |
+          ranges::views::transform([&](const Point & p) {
+            if (auto enemies = world_model->theirs.getAvailableRobots(); not enemies.empty()) {
+              return std::make_pair(
+                world_model
+                  ->getNearestRobotWithDistanceFromSegment({p, world_model->ball.pos}, enemies)
+                  .second,
+                p);
+            } else {
+              return std::make_pair(0.0, robot_pos);
+            }
+          }) |
+          ranges::to<std::vector>();
+
+        auto [min_score, max_score] = ranges::minmax_element(
+          points_with_score, [](const auto & a, const auto & b) { return a.first < b.first; });
+
+        for (const auto & [score, point] : points_with_score) {
+          SvgCircleBuilder circle;
+          circle.center(point).radius(0.05).fill(
+            "red", (score - min_score->first) / (max_score->first - min_score->first));
+          visualizer->add(circle.getSvgString());
+        }
+        SvgCircleBuilder circle;
+        circle.center(max_score->second).radius(0.05).fill("red").stroke("black").strokeWidth(20);
+        visualizer->add(circle.getSvgString());
+        receive_skill->commander().setTargetPosition(max_score->second).lookAtBall();
+        return {PlannerBase::Status::SUCCESS, {receive_skill->getRobotCommand()}};
+      }
     } else {
       return {PlannerBase::Status::RUNNING, {}};
     }
