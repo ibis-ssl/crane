490cc2b9171000594fd5270dc64c01d62ee12aca
Kotaro Yoshimoto <pythagora.yoshimoto@gmail.com>
2025-02-28 02:45:03 +0900

    ball_model.hppをrangesで書き直し

--- utility/crane_basics/include/crane_basics/ball_model.hpp
+++ utility/crane_basics/include/crane_basics/ball_model.hpp
@@ -9,6 +9,8 @@
 
 #include <crane_basics/boost_geometry.hpp>
 #include <optional>
+#include <range/v3/all.hpp>
+#include <utility>
 #include <vector>
 
 namespace crane
@@ -26,27 +28,22 @@ inline std::optional<Point> getFutureBallPosition(
 inline std::vector<double> generateSequence(double start, double end, double step)
 {
   int size = (end - start) / step + 1;
-  std::vector<double> sequence(size);
-  double current = start;
-  std::generate_n(sequence.begin(), size, [&current, step]() mutable {
-    double temp = current;
-    current += step;
-    return temp;
-  });
-  return sequence;
+  return ranges::views::iota(0, size) |
+         ranges::views::transform([&](int i) -> double { return start + i * step; }) |
+         ranges::to<std::vector>();
 }
 
 inline std::vector<std::pair<Point, double>> getBallSequence(
   double t_horizon, double t_step, Point ball_pos, Point ball_vel)
 {
-  std::vector<double> t_ball_sequence = generateSequence(0.0, t_horizon, t_step);
-  std::vector<std::pair<Point, double>> ball_sequence;
-  for (auto t_ball : t_ball_sequence) {
-    if (auto p_ball = getFutureBallPosition(ball_pos, ball_vel, t_ball); p_ball.has_value()) {
-      ball_sequence.push_back({p_ball.value(), t_ball});
-    }
-  }
-  return ball_sequence;
+  auto t_ball_sequence = generateSequence(0.0, t_horizon, t_step);
+  return t_ball_sequence | ranges::views::transform([&](double t) {
+           return std::make_pair(getFutureBallPosition(ball_pos, ball_vel, t), t);
+         }) |
+         ranges::views::filter([](const auto & p) { return p.first.has_value(); }) |
+         ranges::views::transform(
+           [](const auto & p) { return std::make_pair(p.first.value(), p.second); }) |
+         ranges::to<std::vector>();
 }
 }  // namespace crane
 #endif  // CRANE_BASICS__BALL_MODEL_HPP_
