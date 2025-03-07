0d1bc8f6d63666573659454f0efd1da5e3a1970a
Kotaro Yoshimoto <pythagora.yoshimoto@gmail.com>
2025-03-08 00:22:31 +0900

    コメントアウトの仕方が悪く、大量のメモリを消費してしまっていたのを修正

--- crane_world_model_publisher/src/world_model_publisher.cpp
+++ crane_world_model_publisher/src/world_model_publisher.cpp
@@ -286,9 +286,9 @@ void WorldModelPublisherComponent::postProcessWorldModel(WorldModelWrapper::Shar
   ranges::for_each(score_grid, [&](const auto & pair) {
     SvgCircleBuilder circle;
     circle.center(pair.first).radius(pair.second * 0.05).stroke("red").strokeWidth(2.);
-    pass_score_visualizer->add(circle.getSvgString());
+    // pass_score_visualizer->add(circle.getSvgString());
   });
-  // pass_score_visualizer->flush();
+  pass_score_visualizer->flush();
 
   auto score_with_bots = our_robots | ranges::views::transform([&](const auto & robot) {
                            return std::make_pair(robot, calc_score(robot->pose.pos));
