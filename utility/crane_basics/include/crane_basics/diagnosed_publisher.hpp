c0fdf446aa6f371bd53bad2abc7f4e88424d2615
Kotaro Yoshimoto <pythagora.yoshimoto@gmail.com>
2025-03-16 19:14:23 +0900

    diagnostic_updater::DiagnosedPublisherのラッパクラスを作成

---
+++ utility/crane_basics/include/crane_basics/diagnosed_publisher.hpp
@@ -0,0 +1,64 @@
+// Copyright (c) 2025 ibis-ssl
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+#ifndef CRANE_BASICS__DIAGNOSED_PUBLISHER_HPP_
+#define CRANE_BASICS__DIAGNOSED_PUBLISHER_HPP_
+
+#include <boost/asio.hpp>
+#include <diagnostic_updater/publisher.hpp>
+#include <exception>
+#include <iostream>
+#include <stdexcept>
+#include <string>
+#include <vector>
+
+namespace crane
+{
+template <typename MessageT>
+class DiagnosedPublisher
+{
+public:
+  template <typename NodeT>
+  DiagnosedPublisher(
+    NodeT & node, const std::string & topic_name, const size_t qos_history_depth,
+    double min_update_frequency, double max_update_frequency,
+    const diagnostic_updater::TimeStampStatusParam & stamp =
+      diagnostic_updater::TimeStampStatusParam())
+  : frequency_status_param{min_update_frequency, max_update_frequency},
+    publisher(node.create_publisher<MessageT>(topic_name, qos_history_depth)),
+    diagnostics_updater(node),
+    diagnose_publisher(
+      publisher, diagnostics_updater,
+      diagnostic_updater::FrequencyStatusParam(
+        &frequency_status_param.min_update_frequency, &frequency_status_param.max_update_frequency),
+      stamp)
+  {
+  }
+
+  struct FrequencyStatusParam
+  {
+    double min_update_frequency;
+    double max_update_frequency;
+  } frequency_status_param;
+
+  void publish(typename MessageT::UniquePtr message)
+  {
+    diagnose_publisher.publish(std::move(message));
+  }
+
+  void publish(const MessageT & message) { diagnose_publisher.publish(message); }
+
+private:
+  typename rclcpp::Publisher<MessageT>::SharedPtr publisher;
+
+  diagnostic_updater::Updater diagnostics_updater;
+
+  diagnostic_updater::DiagnosedPublisher diagnose_publisher;
+};
+
+}  // namespace crane
+
+#endif  // CRANE_BASICS__DIAGNOSED_PUBLISHER_HPP_
