4b0a43a8d2fdbfd826ac05a9cd61b57493fe30ab
Kotaro Yoshimoto <pythagora.yoshimoto@gmail.com>
2025-04-17 06:46:13 +0900

    速度によって障害物回避の度合いを調整

--- crane_local_planner/src/rvo2_planner.cpp
+++ crane_local_planner/src/rvo2_planner.cpp
@@ -74,6 +74,13 @@ void RVO2Planner::reflectWorldToRVOSim(crane_msgs::msg::RobotCommands & msg)
     rvo_sim->setAgentPosition(
       command.robot_id, RVO::Vector2(command.current_pose.x, command.current_pose.y));
     rvo_sim->setAgentPrefVelocity(command.robot_id, RVO::Vector2(0.f, 0.f));
+    if (auto vel = std::hypot(command.current_velocity.x, command.current_velocity.y); vel < 0.5) {
+      rvo_sim->setAgentRadius(command.robot_id, 0.05f);
+    }else if (vel < 1.5) {
+      rvo_sim->setAgentRadius(command.robot_id, 0.09f);
+    }else {
+      rvo_sim->setAgentRadius(command.robot_id, 0.18f);
+    }
 
     auto robot = world_model->getOurRobot(command.robot_id);
 
