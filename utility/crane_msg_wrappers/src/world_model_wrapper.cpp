322084e58a740db058389c5dcdb2638de4d1e00f
Kotaro Yoshimoto <pythagora.yoshimoto@gmail.com>
2025-05-04 13:32:47 +0900

    SlackTimeのオフセットを0.3秒増やす

--- utility/crane_msg_wrappers/src/world_model_wrapper.cpp
+++ utility/crane_msg_wrappers/src/world_model_wrapper.cpp
@@ -504,7 +504,7 @@ auto WorldModelWrapper::BallOwnerCalculator::calculateScore(
   RobotWithScore score;
   score.robot = robot;
   auto [min_slack, max_slack] = world_model->getMinMaxSlackInterceptPointAndSlackTime(
-    {robot}, 3.0, 0.1, 0.5, 2.5, 5.0, ball_distance_horizon);
+    {robot}, 4.0, 0.1, 0.2, 2.5, 5.0, ball_distance_horizon);
   if (min_slack.has_value() && min_slack.value().slack_time > 0.) {
     score.min_slack = min_slack->slack_time;
     score.min_slack_pos_distance = (min_slack->intercept_point - world_model->ball.pos).norm();
