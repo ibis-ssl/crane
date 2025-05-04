8ab5ea9ea3df36cbc21b36945bc88b2705f37207
Kotaro Yoshimoto <pythagora.yoshimoto@gmail.com>
2025-05-05 06:29:34 +0900

    パラチュン

--- crane_bringup/launch/crane.launch.py
+++ crane_bringup/launch/crane.launch.py
@@ -51,7 +51,7 @@ def generate_launch_description():
                 "simple_ai", default_value="false", description="SimpleAIモードのフラグ"
             ),
             DeclareLaunchArgument(
-                "max_vel", default_value="7.0", description="ロボットの最大速度"
+                "max_vel", default_value="8.0", description="ロボットの最大速度"
             ),
             DeclareLaunchArgument(
                 "speak", default_value="false", description="音声ノードの起動フラグ"
@@ -91,7 +91,7 @@ def generate_launch_description():
             ),
             DeclareLaunchArgument(
                 "robot_max_vel_for_prediction",
-                default_value="5.0",
+                default_value="4.0",
                 description="slack timeの計算などに用いられるロボットの最大速度",
             ),
             Node(
