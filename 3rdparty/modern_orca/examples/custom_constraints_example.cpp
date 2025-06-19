383b587609741827d89839d1d5af97262e137d64
Kotaro Yoshimoto <pythagora.yoshimoto@gmail.com>
2025-06-19 19:17:22 +0900

    ライセンス表記をアップデート

--- 3rdparty/modern_orca/examples/custom_constraints_example.cpp
+++ 3rdparty/modern_orca/examples/custom_constraints_example.cpp
@@ -1,3 +1,9 @@
+// Copyright (c) 2025 ibis-ssl
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
 #include <cmath>
 #include <iostream>
 #include <modern_orca/modern_orca.hpp>
--- 3rdparty/modern_orca/include/modern_orca/concepts.hpp
+++ 3rdparty/modern_orca/include/modern_orca/concepts.hpp
@@ -1,4 +1,9 @@
-// Copyright 2024 ibis-ssl
+// Copyright (c) 2025 ibis-ssl
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
 #pragma once
 
 #include <concepts>
