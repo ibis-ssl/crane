3f94af8f905f0d5426c5132f87332d8f341133c7
Kotaro Yoshimoto <pythagora.yoshimoto@gmail.com>
2025-10-19 20:44:49 +0900

    [テスト]crane_senderの著作権表記を補完

--- crane_sender/src/sender_base.cpp
+++ crane_sender/src/sender_base.cpp
@@ -1,4 +1,8 @@
 // Copyright (c) 2025 ibis-ssl
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
 
 #include "crane_sender/sender_base.hpp"
 
