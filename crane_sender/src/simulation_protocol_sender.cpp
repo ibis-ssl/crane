722c4f480093ce6cd896de3d6dfd7fbc23225317
Kotaro Yoshimoto <pythagora.yoshimoto@gmail.com>
2025-11-02 16:23:22 +0900

    crane_senderビルド最適化 (#958)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- crane_sender/src/simulation_protocol_sender.cpp
+++ crane_sender/src/simulation_protocol_sender.cpp
@@ -1,10 +1,10 @@
-// Copyright (c) 2022 ibis-ssl
+// Copyright (c) 2025 ibis-ssl
 //
 // Use of this source code is governed by an MIT-style
 // license that can be found in the LICENSE file or at
 // https://opensource.org/licenses/MIT.
 
-#include "crane_sender/sim_sender.hpp"
+#include "crane_sender/simulation_protocol_sender.hpp"
 
 #include <crane_geometry/boost_geometry.hpp>
 #include <crane_geometry/geometry_operations.hpp>
@@ -12,36 +12,23 @@
 
 namespace crane
 {
-void SimSenderComponent::sendCommands(const crane_msgs::msg::RobotCommands & msg)
+void SimulationProtocolSenderComponent::sendCommands(const crane_msgs::msg::RobotCommands & msg)
 {
-  //    if (checkNan(msg)) {
-  //      return;
-  //    }
+  auto & sender = msg.is_yellow ? yellow_sender : blue_sender;
 
-  //    // 座標変換（ワールド->各ロボット）
-  //    double vx = msg.target_velocity.x;
-  //    double vy = msg.target_velocity.y;
-  //    double omega = command.target_theta - command.current_pose.theta;
-  //    double theta = command.current_pose.theta + omega * delay_s;
-  //    command.target_velocity.x = vx * cos(-theta) - vy * sin(-theta);
-  //    command.target_velocity.y = vx * sin(-theta) + vy * cos(-theta);
-
-  //    command.target_velocity.theta =
-  //      -theta_controllers.at(command.robot_id)
-  //         .update(getAngleDiff(command.current_pose.theta, command.target_theta), 0.033);
-
-  const double MAX_KICK_SPEED = 8.0;  // m/s
-  robocup_ssl_msgs::msg::Commands commands;
-  commands.isteamyellow = msg.is_yellow;
-  commands.timestamp = msg.header.stamp.sec;
+  RobotControl packet;
 
   for (const auto & command : msg.robot_commands) {
-    robocup_ssl_msgs::msg::RobotCommand cmd;
-    cmd.set__id(command.robot_id);
+    auto cmd = packet.add_robot_commands();
+    cmd->set_id(command.robot_id);
+
+    auto move_command = new RobotMoveCommand();
+    auto move_local_velocity = new MoveLocalVelocity();
+
     float omega = theta_controllers[command.robot_id].update(
       -getAngleDiff(command.current_pose.theta, command.target_theta), 0.033);
     omega = std::clamp(omega, -command.omega_limit, command.omega_limit);
-    cmd.set__velangular(omega);
+    move_local_velocity->set_angular(omega);
 
     switch (command.control_mode) {
       case crane_msgs::msg::RobotCommand::LOCAL_CAMERA_MODE: {
@@ -49,8 +36,8 @@ void SimSenderComponent::sendCommands(const crane_msgs::msg::RobotCommands & msg
         double vy = command.local_camera_mode.front().target_global_vy;
 
         double theta = command.current_pose.theta + omega * delay_s;
-        cmd.set__veltangent(vx * cos(-theta) - vy * sin(-theta));
-        cmd.set__velnormal(vx * sin(-theta) + vy * cos(-theta));
+        move_local_velocity->set_forward(vx * cos(-theta) - vy * sin(-theta));
+        move_local_velocity->set_left(vx * sin(-theta) + vy * cos(-theta));
       } break;
       case crane_msgs::msg::RobotCommand::POSITION_TARGET_MODE: {
         Velocity vel;
@@ -72,15 +59,15 @@ void SimSenderComponent::sendCommands(const crane_msgs::msg::RobotCommands & msg
         vel_local << vel.x() * cos(-command.current_pose.theta) -
                        vel.y() * sin(-command.current_pose.theta),
           vel.x() * sin(-command.current_pose.theta) + vel.y() * cos(-command.current_pose.theta);
-        cmd.set__veltangent(vel_local.x());
-        cmd.set__velnormal(vel_local.y());
+        move_local_velocity->set_forward(vel_local.x());
+        move_local_velocity->set_left(vel_local.y());
       } break;
       case crane_msgs::msg::RobotCommand::SIMPLE_VELOCITY_TARGET_MODE: {
         double vx = command.simple_velocity_target_mode.front().target_vx;
         double vy = command.simple_velocity_target_mode.front().target_vy;
         double theta = command.current_pose.theta + omega * delay_s;
-        cmd.set__veltangent(vx * cos(-theta) - vy * sin(-theta));
-        cmd.set__velnormal(vx * sin(-theta) + vy * cos(-theta));
+        move_local_velocity->set_forward(vx * cos(-theta) - vy * sin(-theta));
+        move_local_velocity->set_left(vx * sin(-theta) + vy * cos(-theta));
       } break;
       case crane_msgs::msg::RobotCommand::POLAR_VELOCITY_TARGET_MODE: {
         double v_r = command.polar_velocity_target_mode.front().target_velocity_r;
@@ -89,8 +76,8 @@ void SimSenderComponent::sendCommands(const crane_msgs::msg::RobotCommands & msg
           command.polar_velocity_target_mode.front().target_velocity_theta - current_theta;
         double vx = v_r * cos(velocity_theta);
         double vy = v_r * sin(velocity_theta);
-        cmd.set__veltangent(vx);
-        cmd.set__velnormal(vy);
+        move_local_velocity->set_forward(vx);
+        move_local_velocity->set_left(vy);
       } break;
       default:
         std::cout << "Invalid control mode" << std::endl;
@@ -99,38 +86,33 @@ void SimSenderComponent::sendCommands(const crane_msgs::msg::RobotCommands & msg
 
     // ストップ
     if (command.stop_flag) {
-      cmd.set__veltangent(0);
-      cmd.set__velnormal(0);
-      cmd.set__velangular(0);
+      move_local_velocity->set_forward(0);
+      move_local_velocity->set_left(0);
+      move_local_velocity->set_angular(0);
     }
 
+    move_command->set_allocated_local_velocity(move_local_velocity);
+    cmd->set_allocated_move_command(move_command);
+
     // キック速度
+    constexpr double MAX_KICK_SPEED = 20.0;  // m/s
     double kick_speed = MAX_KICK_SPEED * command.kick_power;
 
     // チップキック
     if (command.chip_enable) {
-      cmd.set__kickspeedx(kick_speed * 0.5);
-      cmd.set__kickspeedz(kick_speed * 0.5);
+      cmd->set_kick_angle(chip_angle_deg * M_PI / 180.);
+      cmd->set_kick_speed(kick_speed * 0.5);
     } else {
-      cmd.set__kickspeedx(kick_speed * 1.0);
-      cmd.set__kickspeedz(0);
+      cmd->set_kick_angle(0.);
+      cmd->set_kick_speed(kick_speed * 1.0);
     }
 
-    // ドリブル
-    cmd.set__spinner(command.dribble_power > 0);
-
-    // タイヤ個別に速度設定しない
-    cmd.set__wheelsspeed(false);
-
-    if (no_movement) {
-      cmd.set__spinner(false);
-    }
-    commands.robot_commands.emplace_back(cmd);
+    // ドリブル(単位：rpm)
+    cmd->set_dribbler_speed(command.dribble_power * 1000.);
   }
 
-  pub_commands.publish(commands);
+  std::string output;
+  packet.SerializeToString(&output);
+  sender->send(output);
 }
 }  // namespace crane
-#include <rclcpp_components/register_node_macro.hpp>
-
-RCLCPP_COMPONENTS_REGISTER_NODE(crane::SimSenderComponent)
