b69b431e0abd5b3ae9eb9a1bf5a46b9da3a18773
Kotaro Yoshimoto <pythagora.yoshimoto@gmail.com>
2025-11-18 09:41:26 +0900

    TODOを削除

--- session/crane_session_controller/test/test.cpp
+++ session/crane_session_controller/test/test.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2023 ibis-ssl
+// Copyright (c) 2025 ibis-ssl
 //
 // Use of this source code is governed by an MIT-style
 // license that can be found in the LICENSE file or at
@@ -7,12 +7,6 @@
 #include <gtest/gtest.h>
 
 #include "crane_session_controller/session_controller.hpp"
-
-// TODO: 以下のメソッドに対する単体テストを追加する（依存性注入パターン導入後）
-// - getAssignedRobotIds(): プランナーから正しくロボットIDを取得できるか
-// - buildAssignmentLog(): 割当状況のログ文字列が正しく生成されるか
-// - logAssignmentIfChanged(): 変更時のみログ出力されるか
-// - tryAssignRobotToPlanner(): ロボット割り当てが正しく動作するか、エラーハンドリングが正しく機能するか
 
 TEST(SessionController, dummy_test)
 {
