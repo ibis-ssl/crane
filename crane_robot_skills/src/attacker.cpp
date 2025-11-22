1d88829ace893f83b2bc42ec2d0bf78037decc66
Kotaro Yoshimoto <pythagora.yoshimoto@gmail.com>
2025-11-22 16:50:33 +0900

    冗長なclearMaxVelocityFactorsを削除

--- crane_robot_skills/src/attacker.cpp
+++ crane_robot_skills/src/attacker.cpp
@@ -53,7 +53,6 @@ void Attacker::initialize()
   // ので自分への遷移関数で初期化処理を実装
   addTransition(AttackerState::ENTRY_POINT, AttackerState::ENTRY_POINT, [this]() -> bool {
     pass_receiver_id = std::nullopt;
-    command->clearMaxVelocityFactors();
     receive_skill.clearVisualizer();
     kick_skill.clearVisualizer();
     goal_kick_skill.clearVisualizer();
