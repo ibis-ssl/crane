bd4b137d5343e42ec2d1d08f5a0106627075417c
Kotaro Yoshimoto <pythagora.yoshimoto@gmail.com>
2026-01-08 03:07:53 +0900

    調整

--- crane_local_planner/include/crane_local_planner/rvo2_planner.hpp
+++ crane_local_planner/include/crane_local_planner/rvo2_planner.hpp
@@ -67,7 +67,7 @@ private:
   float RVO_RADIUS = 0.09f;
   float RVO_MAX_SPEED = 10.0f;
 
-  double MAX_VEL = 4.0;
+  double MAX_VEL = 5.0;
   double ACCELERATION = 4.0;
   double STOP_STATE_MAX_VELOCITY = 1.0;
   // 加速度は減速度の何倍にするかという係数
