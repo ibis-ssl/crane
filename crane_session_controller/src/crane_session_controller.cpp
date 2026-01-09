a566ac627409227dda5a294f74e3737940951c7a
Kotaro Yoshimoto <pythagora.yoshimoto@gmail.com>
2026-01-09 09:40:29 +0900

    起動時に空イベントが発火するバグを修正 (#1037)

--- crane_session_controller/src/crane_session_controller.cpp
+++ crane_session_controller/src/crane_session_controller.cpp
@@ -67,6 +67,9 @@ SessionControllerComponent::SessionControllerComponent(const rclcpp::NodeOptions
   using std::chrono::operator""ms;
   timer = rclcpp::create_timer(this, get_clock(), 100ms, [&]() {
     ScopedTimer timer(timer_process_time_pub);
+    if (play_situation.command.name.empty()) {
+      return;  // PlaySituation未受信時はスキップ
+    }
     assign(play_situation.command.name);
   });
 
@@ -111,7 +114,7 @@ SessionControllerComponent::SessionControllerComponent(const rclcpp::NodeOptions
       robot_changed = true;
     }
 
-    if (robot_changed) {
+    if (robot_changed && !play_situation.command.name.empty()) {
       assign(play_situation.command.name);
     }
 
@@ -274,9 +277,9 @@ auto SessionControllerComponent::logAssignmentIfChanged(const std::string & curr
 {
   if (current_assignment != prev_assignment_log_) {
     if (current_assignment.empty()) {
-      RCLCPP_INFO(get_logger(), "ロボット割当: なし");
+      RCLCPP_DEBUG(get_logger(), "ロボット割当: なし");
     } else {
-      RCLCPP_INFO(get_logger(), "ロボット割当: %s", current_assignment.c_str());
+      RCLCPP_DEBUG(get_logger(), "ロボット割当: %s", current_assignment.c_str());
     }
     prev_assignment_log_ = current_assignment;
   }
